507e550e1e79221337ef6e434188e726cd3525fa
Dread Knight <dk.vali@gmail.com>
2012-01-27 01:36:06 +0200

    homepage improvements

--- index.php
+++ index.php
@@ -1,39 +1,22 @@
 <?
 $page_title = "Ancient Beast";
-$style =
-".title {
-	font-size: 30px;
-	overflow: hidden;
-}
-.bold {
-font-weight:bold;
-}
-";
 include("header.php");
-echo $start_div;
-?>
-<!--main area-->
-<table>
-<tr>
-<!--left column-->
-<td style="vertical-align:top;">
+echo $start_div; ?>
+<!--main area
+<td style="vertical-align:top;">-->
+<div style="background-image:url('/images/trailer.jpg');"><center>
+<iframe width="295" height="200" src="http://www.youtube.com/embed/KBS03PBHtqQ?rel=0" frameborder="0" allowfullscreen></iframe>
+</center></div>
+<? echo $separator; ?>
 <center>
 <a href="http://www.FreezingMoon.org" onMouseOver="document.FM.src='http://www.FreezingMoon.org/images/Freezing Moon.png';" onMouseOut="document.FM.src='http://www.FreezingMoon.org/images/Freezing Moon desaturated.png';" target="_blank"> 
 <img src="http://www.FreezingMoon.org/images/Freezing Moon desaturated.png" name="FM" style="width:90%;" title="Developed by Freezing Moon"></a><br>
 <span>- presents -</span></center>
 <p style="text-align:justify; margin-right:4px;">
-<span class="bold">Ancient Beast</span>, commonly abreviated <span class="bold">AB</span>, is an online multiplayer game, a hybrid combining turn based strategy with role-playing elements, based on an exciting dark fantasy universe infused with rich Norse mythology, featuring 7 distinctive realms containing a wide variety of creatures to master or defeat, each with it's own unique and exciting set of skills, upgrades and evolutions that are brought to life by nicely crafted prerendered 3d graphics.
+<span class="bold">Ancient Beast</span>, commonly abreviated <span class="bold">AB</span>, is a hybrid game that combines turn based strategy with role-playing elements along with mini-games of other genres. It is based on an exciting dark fantasy universe infused with mythology, featuring 7 distinctive realms containing a wide variety of creatures to master or defeat, each with it's own unique and exciting set of skills, upgrades and evolutions that are brought to life by nicely crafted prerendered 3d graphics.
 <br><br>
 Ancient Beast is developed using web programming languages such as HTML 5 and javascript in order to be playable right from any modern browser on pretty much any operating system and hardware, without having to download and install any plugins. It was carefuly designed to be fast, easy to learn and fun.
 </p>
-</td>
-<td width=1%></td>
-<!--right column-->
-<td style="vertical-align: top;">
-<iframe width="420" height="345" src="http://www.youtube.com/embed/KBS03PBHtqQ" frameborder="0" allowfullscreen></iframe>
-</td>
-</tr>
-</table>
 <? echo $separator;
 include("contribute.php");
 echo $end_div . $the_end; ?>
