13a2ae45e3b604d9cf806eeb8badceafc7bdfd55
Dread Knight <dk.vali@gmail.com>
2012-01-27 03:14:04 +0200

    more refinements to homepage

--- index.php
+++ index.php
@@ -1,22 +1,32 @@
-<?
-$page_title = "Ancient Beast";
+<? $page_title = "Ancient Beast";
 include("header.php");
 echo $start_div; ?>
-<!--main area
-<td style="vertical-align:top;">-->
-<div style="background-image:url('/images/trailer.jpg');"><center>
-<iframe width="295" height="200" src="http://www.youtube.com/embed/KBS03PBHtqQ?rel=0" frameborder="0" allowfullscreen></iframe>
-</center></div>
-<? echo $separator; ?>
+<table style="background: url('/images/trailer.jpg') no-repeat center top; height: 400px;">
+<tr><td width=25%>
 <center>
+		<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+		<input type="hidden" name="cmd" value="_s-xclick">
+		<input type="hidden" name="hosted_button_id" value="FU5PCMX33AVJE">
+		<input type="image" src="/images/paypal.png"  name="submit" alt="PayPal - The safer, easier way to pay online!">
+		<img alt=""  src="https://www.paypal.com/en_US/i/scr/pixel.gif" style="width: 1px; height: 1px; display:none;"></form><br><br><br><br>
+	<a class="FlattrButton" style="display:none;" href="http://www.AncientBeast.com"></a><br><br><br><br><br>
+	<script type="text/javascript" src="http://apis.google.com/js/plusone.js"></script><g:plusone size="tall"></g:plusone>
+</center>
+</td><td width=50%>
+<center><iframe width="480" height="360" src="http://www.youtube.com/embed/KBS03PBHtqQ?rel=0" frameborder="0" allowfullscreen></iframe></center>
+</td><td width=25%>
+<center>
+	<a href='http://www.facebook.com/AncientBeast' target='_blank'><img src='/images/facebook.png'></a><br><br><br><br><br>
+	<a href='http://twitter.com/AncientBeast' target='_blank'><img src='/images/twitter.png'></a><br><br><br><br><br>
+	<a href='http://feeds.feedburner.com/AncientBeast' target='_blank'><img src='/images/rss.png'></a>
+</center>
+</td></tr></table>
+<? echo $separator; ?>
+<!--<center>
 <a href="http://www.FreezingMoon.org" onMouseOver="document.FM.src='http://www.FreezingMoon.org/images/Freezing Moon.png';" onMouseOut="document.FM.src='http://www.FreezingMoon.org/images/Freezing Moon desaturated.png';" target="_blank"> 
-<img src="http://www.FreezingMoon.org/images/Freezing Moon desaturated.png" name="FM" style="width:90%;" title="Developed by Freezing Moon"></a><br>
-<span>- presents -</span></center>
+<img src="http://www.FreezingMoon.org/images/Freezing Moon desaturated.png" name="FM" style="width:30%;" title="Developed by Freezing Moon"></a><br>
+<span>- presents -</span></center>-->
 <p style="text-align:justify; margin-right:4px;">
-<span class="bold">Ancient Beast</span>, commonly abreviated <span class="bold">AB</span>, is a hybrid game that combines turn based strategy with role-playing elements along with mini-games of other genres. It is based on an exciting dark fantasy universe infused with mythology, featuring 7 distinctive realms containing a wide variety of creatures to master or defeat, each with it's own unique and exciting set of skills, upgrades and evolutions that are brought to life by nicely crafted prerendered 3d graphics.
-<br><br>
-Ancient Beast is developed using web programming languages such as HTML 5 and javascript in order to be playable right from any modern browser on pretty much any operating system and hardware, without having to download and install any plugins. It was carefuly designed to be fast, easy to learn and fun.
+<span class="bold">Ancient Beast</span> combines turn based strategy with role-playing elements along with mini-games of other genres. It is based on an exciting dark fantasy universe infused with mythology, featuring 7 distinctive realms containing a wide variety of creatures to master or defeat, each with it's own unique and exciting set of skills, upgrades and evolutions that are brought to life by nicely crafted prerendered 3d graphics.
 </p>
-<? echo $separator;
-include("contribute.php");
-echo $end_div . $the_end; ?>
+<? echo $end_div . $the_end; ?>
