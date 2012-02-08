d929a84251ae8e01b95b18ec4ba6744df1c89014
Dread Knight <dk.vali@gmail.com>
2012-02-08 11:42:03 +0200

    added info about github to the development section of the manual

--- manual/index.php
+++ manual/index.php
@@ -145,14 +145,14 @@ Underwold. Back in the day there used to be underground secret facilities that w
 <b>Pipeline</b>
 <p>
 The project is developed with the use of free open source cross platform applications and freeware services.
-<a href="http://wuala.com" target="_blank"><b>Wuala</b></a> comes in very handy when working with files collaboratively. You can find our group over <a href="http://wuala.com/AncientBeast" target="_blank"><b>here</b></a> which contains all the project's files.
+<a href="http://wuala.com" target="_blank"><b>Wuala</b></a> comes in very handy when working with files collaboratively. You can find our group over <a href="http://wuala.com/AncientBeast" target="_blank"><b>here</b></a> which contains all the project's assets and sources, while <a href="https://github.com/FreezingMoon/AncientBeast" target="_blank"><b>Github</b> handles the code part and stores the final assets.
 <a href="http://blender.org" target="_blank"><b>Blender</b></a> is being used for creating most of the assets, such as structures and creatures and their animations, which are prerendered into sprites and sprite-sheets as well as for other tasks.
 <a href="http://mypaint.intilinux.com" target="_blank"><b>My Paint</b></a> along with <a href="http://gimp.org" target="_blank"><b>Gimp</b></a> are being used for icons, concept art, texturing and the creation of some of the battle grounds.
 <a href="http://al.chemy.org" target="_blank"><b>Alchemy</b></a> can be a very useful tool for finding inspiration when creating creature artwork.
 </p>
 <table style="font-size:18px; font-weight:bold; margin:0; padding:0; margin-left:auto; margin-right:auto; text-align:center;"><tr>
 <?php
-$tools = array("wuala"=>"http://wuala.com", "blender"=>"http://blender.org", "gimp"=>"http://gimp.org", "mypaint"=>"http://mypaint.intilinux.com", "alchemy"=>"http://al.chemy.org");
+$tools = array("github"=>"https://github.com/FreezingMoon/AncientBeast", "wuala"=>"http://www.wuala.com/AncientBeast/", "blender"=>"http://blender.org", "gimp"=>"http://gimp.org", "mypaint"=>"http://mypaint.intilinux.com", "alchemy"=>"http://al.chemy.org");
 foreach ($tools as $toolName => $toolLink) {
 	echo "<td><a href='$toolLink' target='_blank' class='lighten'><img src='$toolName.png' style='display:block;'>".ucfirst($toolName)."</a></td>";
 }
