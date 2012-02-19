06486509a007bd04e255d3d060300b72fdf101f1
Nehmulos <Nehmulos@inbox.com>
2012-02-19 17:16:41 +0100

    added multiply function; added convert to Units function;

--- game/arenas/Vertex.js
+++ game/arenas/Vertex.js
@@ -11,6 +11,15 @@ Vertex.prototype.substract = function(other) {
     return new Vertex(this.x - other.x, this.y - other.y);
 }
 
+Vertex.prototype.multiply = function(other) {
+    return new Vertex(this.x * other.x, this.y * other.y);
+}
+
 Vertex.prototype.toScreenSpace = function(renderer) {
     return new Vertex(renderer.screenSpace(this.x), renderer.screenSpace(this.y));
 }
+
+Vertex.prototype.toUnitSpace = function(renderer) {
+    return new Vertex(renderer.unitSpace(this.x), renderer.unitSpace(this.y));
+}
+
