d0ddf60b6eb466e673fc52afa8ac7b2cce446b58
Dread Knight <dk.vali@gmail.com>
2012-02-20 15:44:43 +0200

    new bitcoin address for better tracking of funds

--- bitcoin/index.php
+++ bitcoin/index.php
@@ -11,7 +11,7 @@ echo $start_div; ?>
 <p class="center">
 <iframe width="560" height="315" src="http://www.youtube.com/embed/Um63OQz3bjo" frameborder="0" allowfullscreen></iframe></p>
 <p class="center">Feel free to send your <a href="http://www.bitcoin.org" style="font-weight: bold;"><img src="bitcoin.ico"> bitcoin</a> donation over at:</p>
-<p class="center" style="font-weight: bold;">1QHJSbZMxhBsPxP7ZbjGrbhznfL8tCEMCM</p>
+<p class="center" style="font-weight: bold;">1Gpa3NKn8nR9ipXPZbwkjYxqZX3cmz7q97</p>
 <p class="center">Any amount we receive helps us reaching our goals. We thank you!</p>
 <?php echo $separator; ?>
 <div class="center">
