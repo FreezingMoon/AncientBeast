a7cf4c1eb1d6a3f7eea45bb6a4575c19464d7618
Dobz <comcal116@gmail.com>
2012-03-13 17:34:57 +1100

    Fix working directory issue again

--- bestiary/index.php
+++ bestiary/index.php
@@ -2,7 +2,7 @@
 include("../header.php");
 include("../global.php");
 echo "$start_div<div style='text-align:center; position:absolute;' id='▣'>";
-foreach(range(1, 7) as $x) echo "<div style=\"background:url('" . $WorkingDir . "/creatures/$x/avatar.jpg'); background-size: 100%; display: inline-block;\"><a href=\"?id=$x#\" class=\"lighten\"><img class=\"img lighten\" src=\"frame.png\"></a></div>";
+foreach(range(1, 7) as $x) echo "<div style=\"background:url('" . $WorkingDir . "creatures/$x/avatar.jpg'); background-size: 100%; display: inline-block;\"><a href=\"?id=$x#\" class=\"lighten\"><img class=\"img lighten\" src=\"frame.png\"></a></div>";
 echo "</div><div style='height:130px;'></div><center>";
 include("cards.php");
 include("progress/index.php");
