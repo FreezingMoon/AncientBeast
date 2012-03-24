2928b84d3b165e2836476a2836074e2142a4b20a
Dread Knight <dk.vali@gmail.com>
2012-03-24 10:10:52 +0200

    more creatures,, artists now credited provided you scroll down a bit and bug fixed when scrolling back through pop-ed out images

--- gallery/index.php
+++ gallery/index.php
@@ -1,6 +1,6 @@
 <?php $page_title = "Ancient Beast - Gallery";
 include("../header.php"); ?>
-<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js"></script>
 <script type="text/javascript" src="http://ancientbeast.com/gallery/fancybox/jquery.fancybox-1.3.4.pack.js"></script>
 <script type="text/javascript" src="http://ancientbeast.com/gallery/fancybox/jquery.easing-1.3.pack.js"></script>
 <script type="text/javascript" src="http://ancientbeast.com/gallery/fancybox/jquery.mousewheel-3.0.4.pack.js"></script>
@@ -20,6 +20,7 @@ $images = scandir("artwork");
 foreach($images as $image) {
 	if($image == "." || $image == "..") continue;
 	$title = substr($image, 0, -4); 
+	$title = str_replace(' by', '<br>by', $title);
 	echo "<a style='text-align:center;' rel='pop' href='artwork/$image' title='$title'><img style='height:200px; margin:5px;' src='artwork/$image' title='$title'></a>";
 } echo "</center>" . $end_div . $the_end; ?>
 
