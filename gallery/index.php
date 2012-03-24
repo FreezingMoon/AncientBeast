9b3438412cb44b58b193ff77d1a6fe36f3f61e9b
Dread Knight <dk.vali@gmail.com>
2012-03-24 10:15:49 +0200

    on hover title fix

--- gallery/index.php
+++ gallery/index.php
@@ -19,8 +19,8 @@ echo $start_div . "<center>";
 $images = scandir("artwork");
 foreach($images as $image) {
 	if($image == "." || $image == "..") continue;
-	$title = substr($image, 0, -4); 
-	$title = str_replace(' by', '<br>by', $title);
-	echo "<a style='text-align:center;' rel='pop' href='artwork/$image' title='$title'><img style='height:200px; margin:5px;' src='artwork/$image' title='$title'></a>";
+	$hover = substr($image, 0, -4); 
+	$title = str_replace(' by', '<br>by', $hover);
+	echo "<a style='text-align:center;' rel='pop' href='artwork/$image' title='$title'><img style='height:200px; margin:5px;' src='artwork/$image' title='$hover'></a>";
 } echo "</center>" . $end_div . $the_end; ?>
 
