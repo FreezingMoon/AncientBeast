2c06c03013cff80bde7ce3916dc6049e39352151
Dread Knight <dk.vali@gmail.com>
2012-03-27 13:16:19 +0300

    got rid of some files and change path for header menu stuff

--- navigation/index.php
+++
@@ -1,8 +0,0 @@
-<nav><table style="font-size:18px; font-weight:bold; margin:0; padding:0; margin-left:auto; margin-right:auto; text-align:center;"><tr>
-<?php
-$menu = array("manual", "bestiary", "items", "game", "chat", "gallery");
-foreach ($menu as &$menuItem) {
-	echo "<td><a href='" . $WorkingDir . "$menuItem' id='$menuItem' class='lighten'><img src='" . $WorkingDir . "navigation/$menuItem.png' width='100' height='100' style='display:block;'>".ucfirst($menuItem)."</a></td>";
-}
-?>
-</tr></table></nav>
