c1d4bd1f13cc96f605fa268c4179ee011ea582a8
Dobz <comcal116@gmail.com>
2012-04-08 21:45:06 +1000

    Implement #28 (linkable gallery images)

--- gallery/index.php
+++ gallery/index.php
@@ -5,22 +5,34 @@ require_once("../header.php"); ?>
 <script type="text/javascript" src="<?php echo $WorkingDir; ?>gallery/fancybox/jquery.easing-1.3.pack.js"></script>
 <script type="text/javascript" src="<?php echo $WorkingDir; ?>gallery/fancybox/jquery.mousewheel-3.0.4.pack.js"></script>
 <link rel="stylesheet" href="<?php echo $WorkingDir; ?>gallery/fancybox/jquery.fancybox-1.3.4.css" type="text/css" media="screen" />
-	<script type="text/javascript">
-		$(document).ready(function() {
-			$("a[rel=pop]").fancybox({
-				'overlayColor'  : 'black',
-				'transitionIn'	: 'elastic',
-				'transitionOut'	: 'elastic'
-			});
+<script type="text/javascript">
+$(document).ready(function() {
+	var basePage = window.location.href.replace(/#.*/, "");
+	$("a[rel=pop]").fancybox({
+		'overlayColor'  : 'black',
+		'transitionIn'	: 'elastic',
+		'transitionOut'	: 'elastic',
+		'onComplete'	: function(array, index) {
+			history.replaceState("", "", basePage + "#id=" + index);
+		},
+		'onClosed'		: function() {
+			history.replaceState("", "", basePage);
+		}
+	});
+	
+	if (/[\#&]id=(\d+)/.test(location.hash))
+		$("#img" + RegExp.$1).trigger("click");
 });
 </script>
 <?php
 echo $start_div . "<center>";
 $images = scandir("artwork");
+$i = 0;
 foreach($images as $image) {
 	if($image == "." || $image == "..") continue;
 	$hover = substr($image, 0, -4); 
 	$title = str_replace(' by', '<br>by', $hover);
-	echo "<a style='text-align:center;' rel='pop' href='artwork/$image' title='$title'><img style='height:200px; margin:5px;' src='artwork/$image' title='$hover'></a>";
+	echo "<a id='img{$i}' style='text-align:center;' rel='pop' href='artwork/$image' title='$title'><img style='height:200px; margin:5px;' src='artwork/$image' title='$hover'></a>";
+	$i++;
 } echo "</center>{$end_div}{$the_end}"; ?>
 
