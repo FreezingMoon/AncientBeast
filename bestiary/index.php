06b0f8e07ac3aebc2c8b57928e90e744bdc2179c
Dread Knight <dk.vali@gmail.com>
2012-04-20 14:31:35 +0300

    small tweaks

--- bestiary/index.php
+++ bestiary/index.php
@@ -1,8 +1,7 @@
 <?php $page_title = "Ancient Beast - Bestiary";
 require_once("../header.php");
 require_once("../config.php");
-require_once("../ico/script.php");
-?>
+require_once("../ico/script.php"); ?>
 <script type="text/javascript" src="<?php echo $WorkingDir; ?>utils/tinybox.js"></script>
 <!--show details of the linked item-->
 <script type="text/javascript">
@@ -23,17 +22,12 @@ window.onload = function() {
 }
 </script>
 
-<?php
-echo
-"$start_div
-<div style='text-align:center; margin-left:-6px;'>";
+<?php echo "$start_div<div style='text-align:center; margin-left:-6px;'>";
 $creatures = "SELECT * FROM ab_creatures ORDER BY sin, lvl";
 $result = mysql_query($creatures) or die(mysql_error());
 while ($row = mysql_fetch_assoc($result)) {
 	echo "<div class=\"lighten\" style=\"background:url('{$WorkingDir}creatures/{$row["id"]}/avatar.jpg'); background-size: 100%; display: inline-block; margin-bottom:-3px; cursor: pointer;\" onclick=\"showId({$row['id']});\">
-	<img style=\"display: block;\" src=\"{$WorkingDir}bestiary/frame.png\" alt=\"frame\">
-</div>";
+	<img style=\"display: block;\" src=\"{$WorkingDir}bestiary/frame.png\" alt=\"frame\"></div>";
 }
 mysql_free_result($result);
-echo "
-</div>{$end_div}{$the_end}"; ?>
+echo "</div>{$end_div}{$the_end}"; ?>
