c013574635ff6064542a3bbaab23b3071d758c2b
Dread Knight <dk.vali@gmail.com>
2012-05-06 03:57:59 +0300

    chat page now includes tutorial and commenting system

--- chat/index.php
+++ chat/index.php
@@ -3,6 +3,10 @@ require_once("../header.php");
 echo $start_div; ?>
 <span style="text-align: center; display: block; font-weight:bold;">
 <p><a href="mailto:DreadKnight@FreezingMoon.org" target="_blank">You might need to wait some time to get a response so consider sending us an <u>e-mail</u> instead.</p></a></span>
-<?php echo $separator; ?>
-<center><iframe src="http://webchat.freenode.net?channels=AncientBeast" width="99%" height="450" style="margin-left:-8px;"></iframe></center>
-<?php echo "{$end_div}{$the_end}"; ?>
+<?php echo $separator;
+echo "<center><iframe src='http://webchat.freenode.net?channels=AncientBeast' width='99%' height='450'></iframe></center>";
+echo $separator;
+echo "<center><iframe src='https://docs.google.com/present/embed?id=dfbbjc3c_24fb4cz7d8&size=l' frameborder='0' width='700' height='559'></iframe></center>";
+echo $separator;
+include("../utils/disqus.php");
+echo $end_div.$the_end; ?>
