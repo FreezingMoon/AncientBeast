6f0222037632663fd37277811ba6dd438b5fb81c
Dread Knight <dk.vali@gmail.com>
2012-05-06 04:38:51 +0300

    changed icon order

--- menu/index.php
+++ menu/index.php
@@ -1,6 +1,6 @@
 <nav><table style="font-size:18px; font-weight:bold; margin:0; padding:0; margin-left:auto; margin-right:auto; text-align:center;"><tr>
 <?php
-$menu = array("bets", "bestiary", "items", "game", "chat", "gallery");
+$menu = array("bestiary", "items", "bets", "game", "chat", "gallery");
 foreach ($menu as &$menuItem) {
 	echo "<td><a href='{$WorkingDir}$menuItem' id='$menuItem' class='lighten'><img alt='".ucfirst($menuItem)."' src='{$WorkingDir}menu/$menuItem.png' width='100' height='100' style='display:block;'>".ucfirst($menuItem)."</a></td>";
 }
