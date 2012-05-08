d5c56b0b86ab34c1a868419835d96cb3f4b0b378
Dobz <comcal116@gmail.com>
2012-05-08 22:45:28 +1000

    Remove old config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-$connect = mysql_connect('host', 'user', 'pass') or
-die ("Server connection issues...");
-mysql_select_db("database");
-mysql_query("SET NAMES 'utf8'");
-
-$WorkingDir = "/";
-$stats = array('health', 'regrowth', 'fatigue', 'energy', 'meditation', 'delay', 'offense', 'defense', 'movement', 'pierce', 'slash', 'crush', 'shock', 'burn', 'frost', 'poison', 'sonic', 'mental');
-?>
