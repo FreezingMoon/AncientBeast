0621df3087bd67e3c7177bc51585535ed2bfa165
Dread Knight <dk.vali@gmail.com>
2012-05-08 05:56:04 +0300

    deprecated filters.php

--- items/index.php
+++ items/index.php
@@ -10,14 +10,18 @@ $style = "
 }";
 require_once("../header.php");
 require_once("../config.php");
-echo $start_div;
-require_once("filters.php");
+require_once("../ico/index.php");
 
+$ICON_LIST = array(HEALTH_ICON, REGROWTH_ICON, FATIGUE_ICON, ENERGY_ICON, MEDITATION_ICON, DELAY_ICON, OFFENSE_ICON, DEFENSE_ICON, MOVEMENT_ICON, PIERCE_ICON, SLASH_ICON, CRUSH_ICON, SHOCK_ICON, BURN_ICON, FROST_ICON, POISON_ICON, SONIC_ICON, MENTAL_ICON);
 $items = 'SELECT * FROM ab_items ORDER BY type, value';
 $result = mysql_query($items) or die(mysql_error());
 
-echo '<a name="grid"><table width=100%><tr>';
+echo $start_div;
+echo "<table width=100%><tr>";
+foreach($ICON_LIST as $x) echo "<th>{$x}</th>";
+echo "</tr></table>".$separator;
 
+echo '<a name="grid"><table width=100%><tr>';
 $i = 0;
 while ($row = mysql_fetch_assoc($result)) {
 	$i++;
@@ -25,10 +29,9 @@ while ($row = mysql_fetch_assoc($result)) {
 <img class=\"fix\" src=\"{$WorkingDir}items/icons/{$row['name']}.png\" style=\"display: block;\"><br>{$row['name']}</a></span></td>";
 	if (($i % 6) == 0) echo '</tr><tr>';
 }
-
 echo '</tr></table></a>'.$end_div;
-$result = mysql_query($items) or die(mysql_error());
 
+$result = mysql_query($items) or die(mysql_error());
 while ($row = mysql_fetch_assoc($result)) {
 	echo $start_div."<table width=100% style='text-align:center;'>";
 	echo "<tr><td style=\"width: 132px;\"><a name=\"{$row['id']}\" href=\"#grid\"><img src=\"{$WorkingDir}items/icons/{$row['name']}.png\"></a></td>";
