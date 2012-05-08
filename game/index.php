3bf7cece1cc35d8b7636b6efb7575f1b9d2dcf28
Dread Knight <dk.vali@gmail.com>
2012-05-08 14:09:26 +0300

    more planning done

--- game/index.php
+++ game/index.php
@@ -2,11 +2,17 @@
 require_once("../header.php");
 require_once("../config.php");
 echo $start_div;
-echo "<center>Coming Soon!</center>";
-//tutorial?
-//casual mode (50 mana, stack of creatures and spells from a sin, no items, low exp and gold)
-//ladder (rankings, full exp and gold)
-//tournament sign-up and status
-//equiped items widget
-//owned creatures/spells/items? (profile, including stats)
+echo "<center>Under HEAVY development!</center>";
+echo $separator;
+echo "casual mode (fixed lvl 50, full stack of creatures from a sin, low exp and gold, no items)";
+echo $separator;
+echo "equiped items widget (6 items, clicking gets you to the items page, focusing on that item)";
+echo $separator;
+echo "ladder (rankings, normal exp and gold)";
+echo $separator;
+echo "hardcore (one life, bonus exp and gold; if defeated, must purchase new life)";
+echo $separator;
+echo "tournament (entry fee, estimated prize and status)";
+//TODO: tutorial? self played demo? replays? (if not signed in)
+//owned creatures/items? (profile, including stats); show purchase suggestions near price range
 echo $end_div.$the_end; ?>
