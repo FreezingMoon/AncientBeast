a5f7312b69bfe37ff76bfb8455c402092f46bdcc
Dread Knight <dk.vali@gmail.com>
2012-05-08 08:52:28 +0300

    dont recall

--- items/index.php
+++ items/index.php
@@ -19,21 +19,20 @@ $result = mysql_query($items) or die(mysql_error());
 echo $start_div;
 echo "<table width=100%><tr>";
 foreach($ICON_LIST as $x) echo "<th>{$x}</th>";
-echo "</tr></table>".$separator;
+echo "</tr></table>$separator";
 
 echo '<a name="grid"><table width=100%><tr>';
 $i = 0;
 while ($row = mysql_fetch_assoc($result)) {
 	$i++;
-	echo "<td class=\"item\"><span style=\"cursor: pointer;\" class=\"lighten\"><a href=\"#{$row['id']}\">
-<img class=\"fix\" src=\"{$WorkingDir}items/icons/{$row['name']}.png\" style=\"display: block;\"><br>{$row['name']}</a></span></td>";
+	echo "<td class=\"item\"><span style=\"cursor: pointer;\" class=\"lighten\"><a href=\"#{$row['id']}\"><img class=\"fix\" src=\"{$WorkingDir}items/icons/{$row['name']}.png\" style=\"display: block;\"><br>{$row['name']}</a></span></td>";
 	if (($i % 6) == 0) echo '</tr><tr>';
 }
-echo '</tr></table></a>'.$end_div;
+echo "</tr></table></a>$end_div";
 
 $result = mysql_query($items) or die(mysql_error());
 while ($row = mysql_fetch_assoc($result)) {
-	echo $start_div."<table width=100% style='text-align:center;'>";
+	echo "$start_div<table width=100% style='text-align:center;'>";
 	echo "<tr><td style=\"width: 132px;\"><a name=\"{$row['id']}\" href=\"#grid\"><img src=\"{$WorkingDir}items/icons/{$row['name']}.png\"></a></td>";
 	echo "<td><table width=100% style='font-size:24px; text-align:left;'><tr>";
 	echo "<td width=40%><a href='#{$row['id']}'>{$row['name']}</a></td>";
@@ -44,7 +43,7 @@ while ($row = mysql_fetch_assoc($result)) {
 	for($i = 0; $i<18; $i++) if($row[$stats[$i]]) echo "<th style='padding:4px;'>{$ICON_LIST[$i]}</th>";
 	echo "</tr><tr>";
 	foreach ($stats as $x) if($row[$x]) echo "<td>{$row[$x]}</td>";
-	echo "</tr></table></td></tr></table></a>".$end_div;
+	echo "</tr></table></td></tr></table></a>$end_div";
 }
 
 echo $start_div;
