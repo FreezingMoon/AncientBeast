386a4f14575aa482edf59d13d15d0bc8d19ce8aa
Dread Knight <dk.vali@gmail.com>
2012-05-08 07:31:05 +0300

    less spacing

--- ico/index.php
+++ ico/index.php
@@ -1,7 +1,4 @@
-<?php
-foreach ($stats as $x)
-	define(strtoupper($x) . '_ICON', '<img src="../ico/'.$x.'.png" title="' . ucfirst($x) . '" onMouseOver="swap(this,\'mouseover_' . $x . '\')" onMouseOut="swap(this,\'normal_' . $x . '\')">'."\n");
-?>
+<?php foreach ($stats as $x) define(strtoupper($x) . '_ICON', '<img src="../ico/'.$x.'.png" title="'.ucfirst($x).'" onMouseOver="swap(this,\'mouseover_'.$x.'\')" onMouseOut="swap(this,\'normal_'.$x.'\')">'."\n"); ?>
 <!--roll over image script-->
 <script type="text/javascript">
 var icons = new Array("<?php echo implode($stats, "\", \""); ?>");
