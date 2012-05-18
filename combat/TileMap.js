271b57ce91b81f36a70a4c45cbeab11f6eeccad1
Nehmulos <Nehmulos@inbox.com>
2012-05-18 19:07:29 +0200

    fixed getTilePosition; save a reference in this.activeTile; various imrovements and fixes;

--- combat/TileMap.js
+++ combat/TileMap.js
@@ -1,10 +1,10 @@
 function TileMap(columns, rows) {
-    
+
 	this.tilesSize = new Vector2D(1.0, 0.7);
 	this.tilesTranslation = new Vector2D(0.3, 4.5);
     this.columns = columns || 18;
     this.rows = rows || 12;
-    this.activeTile = -1;
+    this.activeTile = null;
 
     var hexagon = MathUtils.generateTessellatingHexagon();
     this.tileSeparation = hexagon.tile;
@@ -39,13 +39,34 @@ TileMap.prototype.draw = function(renderer) {
 
 /// @return Vector2D the actual x,y position the tile has on the renderer target.
 TileMap.prototype.getTilePosition = function(tile) {
+	var rowHeight = (this.tilesSize.y*this.tileSeparation.y);
+	var columnWidth = (this.tilesSize.x*this.tileSeparation.x);
+    var tileIndex2D = this.getTileIndex2D(tile);
+	var position = new Vector2D(tileIndex2D.x * columnWidth, tileIndex2D.y * rowHeight);
+	var offsetX = tileIndex2D.y % 2 == 0 ? columnWidth : columnWidth/2;
+	position.x = position.x + offsetX;
+	position = position.add(this.tilesTranslation);
+	position = position.multiply(this.tilesSize);
+/*
+    var position = new Vector2D(this.tilesTranslation.x, this.tilesTranslation.y);
+    var tileIndex2D = this.getTileIndex2D(tile);
+    var offset = new Vector2D(tileIndex2D.y % 2 == 0 ? this.tilesSize.x : this.tilesSize.x * 0.5, 0);
+    position = position.add(offset);
+    var seperationOffset = new Vector2D(tileIndex2D.x * this.tileSeparation.x, tileIndex2D.y * this.tileSeparation.y);
+    position = position.add(seperationOffset);
+    position = position.multiply(this.tilesSize);
+*/
+/*
     var position = new Vector2D(this.tilesTranslation.x, this.tilesTranslation.y);
     var tileIndex2D = this.getTileIndex2D(tile);
     var offset = new Vector2D(tileIndex2D.y % 2 == 0 ? this.tilesSize.x : this.tilesSize.x * 0.5, 0);
 	var translate = new Vector2D(offset.x + tileIndex2D.x * this.tileSeparation.x,
 	                             offset.y + tileIndex2D.y * this.tileSeparation.y);
+    translate = translate.add(this.tilesTranslation);
+
 	position = position.add(translate);
-//    position = position.multiply(this.tilesSize);
+	position = position.multiply(this.tilesSize);
+*/
     return position;
 }
 
@@ -69,7 +90,7 @@ TileMap.prototype.getTileIndex = function(tile) {
 TileMap.prototype.getTileIndex2D = function(tile) {
     var index = this.getTileIndex(tile);
     if (index > -1) {
-        return new Vector2D(index % this.columns, index / this.rows);
+        return new Vector2D(index % this.columns, (index - (index % this.columns))/ this.rows);
     } else {
         return null;
     }
@@ -101,28 +122,43 @@ TileMap.prototype.onMouseMove = function(renderer, mouse) {
     this.mouse = mouse;
 
     var newActiveTile = this.getActiveTile(renderer, mouse);
+    //TODO group if blocks
+        
+	if (this.activeTile != null) {
+	    this.activeTile.filled = false;
+	}
+    
+    // Highlight selected Creature's tiles
+    if (this.activeTile != null && this.activeTile.creature == this.activeCreature && this.activeCreature != null) {
+        this.activeTile.creature.setTileStyle(false, "rgba(65, 145, 250, 0.2)");
+    }
+    
+    if (newActiveTile != null && newActiveTile.creature != null) {
+        newActiveTile.creature.setTileStyle(true, "rgba(65, 145, 250, 0.2)");
+        this.activeCreature = newActiveTile.creature;
+    }
 	
-	if (this.activeTile != -1) {
-	    this.tiles[this.activeTile].filled = false;
+	if (newActiveTile != null) {
+	    newActiveTile.filled = true;
+	    newActiveTile.fillColor = "rgba(250, 145, 65, 0.2)";
+    	this.activeTile = newActiveTile;
+	} else {
+	    this.activeTile = null;
 	}
 	
-	if (newActiveTile != -1) {
-	    this.tiles[newActiveTile].filled = true;
-	}
-	this.activeTile = newActiveTile;
 }
 
 TileMap.prototype.getActiveTile = function(renderer, mouse) {
 	var rowHeight = (this.tilesSize.y*this.tileSeparation.y);
 	var columnWidth = (this.tilesSize.x*this.tileSeparation.x);
-	var activeTile = -1;
+	var activeTile = null;
 	
-	if (this.mouse.x > this.tilesTranslation.x && 
-		this.mouse.y > this.tilesTranslation.y &&
-		this.mouse.x < this.tilesTranslation.x + columnWidth*(this.columns+1) &&
-		this.mouse.y < this.tilesTranslation.y + rowHeight*this.rows ) {
+	if (mouse.x > this.tilesTranslation.x && 
+		mouse.y > this.tilesTranslation.y &&
+		mouse.x < this.tilesTranslation.x + columnWidth*(this.columns+1) &&
+		mouse.y < this.tilesTranslation.y + rowHeight*this.rows ) {
 		
-		var translatedMouse = this.mouse.substract(this.tilesTranslation);
+		var translatedMouse = mouse.substract(this.tilesTranslation);
 		var activeRow = Math.floor(translatedMouse.y / rowHeight) % this.rows;
 		var offsetX = activeRow % 2 == 0 ? columnWidth : columnWidth/2;
 		var activeColumn = Math.floor((translatedMouse.x-offsetX) / columnWidth) % this.columns;
@@ -130,16 +166,16 @@ TileMap.prototype.getActiveTile = function(renderer, mouse) {
 		// ignore if past the offset
 		if ((activeRow % 2 == 0 && translatedMouse.x < columnWidth) ||
 			(activeRow % 2 != 0 && translatedMouse.x-offsetX > columnWidth*this.columns || activeColumn == -1)) {
-			activeTile = -1;
+			activeTile = null;
 			$(renderer.canvas).removeClass("cursorPointer");
 		} else {
-			activeTile = activeRow* this.columns + activeColumn;
+			activeTile = this.tiles[activeRow* this.columns + activeColumn];
 			$(renderer.canvas).addClass("cursorPointer");
 		}
 
 		//console.log(activeRow +","+ activeColumn);
 	} else {
-		activeTile = -1;
+		activeTile = null;
 		$(renderer.canvas).removeClass("cursorPointer");
 	}
 	return activeTile;
