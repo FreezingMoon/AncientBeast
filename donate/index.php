5c7d17823897eaed837b0b619229d0b7034433a3
Dread Knight <dk.vali@gmail.com>
2012-06-17 03:53:10 +0300

    added transparency for bitcoin donations, when you click on the address

--- donate/index.php
+++ donate/index.php
@@ -105,7 +105,7 @@ start_segment(); ?>
 <p id="bitcoin" class="center">
 <iframe width="560" height="315" src="http://www.youtube.com/embed/Um63OQz3bjo" frameborder="0" allowfullscreen></iframe></p>
 <p class="center">Feel free to send your <a href="http://www.bitcoin.org" style="font-weight: bold;"><img src="bitcoin.ico"> bitcoin</a> donation over at:</p>
-<p class="center" style="font-weight: bold;">1Gpa3NKn8nR9ipXPZbwkjYxqZX3cmz7q97</p>
+<p class="center" style="font-weight: bold;"><a href="http://blockexplorer.com/address/1Gpa3NKn8nR9ipXPZbwkjYxqZX3cmz7q97" target="_blank">1Gpa3NKn8nR9ipXPZbwkjYxqZX3cmz7q97</a></p>
 <p class="center">Any amount we receive helps us reaching our goals. We thank you!</p>
 <!-- total/montly amount of bitcoins received -->
 <?php
