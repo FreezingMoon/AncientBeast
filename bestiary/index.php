e01a9d605b9616fe38feff4cff60b34a3529d474
Dread Knight <dk.vali@gmail.com>
2012-06-22 10:18:34 +0300

    tweaks to the grid

--- bestiary/index.php
+++ bestiary/index.php
@@ -10,25 +10,21 @@ $spaceless = str_replace(" ", "_", $r['name']);
 $creatures = "SELECT * FROM ab_creatures ORDER BY sin, lvl";
 $creature_results = db_query($creatures);
 
-start_segment();
-	echo "<div style='text-align:center; margin-left:-6px;'><a name='grid'>";
-	foreach ($creature_results as $r) {
-		$spaceless = str_replace(" ", "_", $r['name']);
-		echo "<div class='lighten' style=\"background:url('{$site_root}bestiary/{$r["name"]}/avatar.jpg'); background-size: 100%; display: inline-block; margin-bottom:-3px; cursor: pointer;\"><a href='#{$spaceless}'><img style='display: block;' src='{$site_root}bestiary/frame.png' alt='frame'></a></div>";
-	}
-	echo "</a></div>";
-end_segment();
+echo "<div style='text-align:center;'><a name='grid'>";
+foreach ($creature_results as $r) {
+	$spaceless = str_replace(" ", "_", $r['name']);
+	echo "<div class='lighten' style=\"background:url('{$site_root}bestiary/{$r["name"]}/avatar.jpg'); background-size: 100%; display: inline-block; margin-bottom:-3px; cursor: pointer;\"><a href='#{$spaceless}'><img style='display: block;' src='{$site_root}bestiary/frame.png' alt='frame'></a></div>";
+}
+echo "</a></div>";
 
-	foreach ($creature_results as $r) {
-		start_segment();
-		cards($r["id"]);
-		progress($r["id"], $r["name"]);
-		end_segment();
-	}
+foreach ($creature_results as $r) {
+	start_segment();
+	cards($r["id"]);
+	progress($r["id"], $r["name"]);
+	end_segment();
+}
 
 start_segment();
 include("../utils/disqus.php");
 end_segment();
-
-end_page();
-?>
+end_page(); ?>
