feaf8267f7c8d7ea0e9bb90c703795d64b2f8e62
Dread Knight <dk.vali@gmail.com>
2012-07-15 15:25:25 +0300

    making room for shop

--- header.php
+++ header.php
@@ -87,7 +87,7 @@ $(document).ready(function(){
 <!--navigation menu-->
 <nav><table style="font-size:18px; font-weight:bold; margin:0; padding:0; margin-left:auto; margin-right:auto; text-align:center;"><tr>
 <?php
-$menu = array("bestiary", "items", "bets", "game", "chat", "gallery");
+$menu = array("bestiary", "items", "shop", "game", "chat", "gallery");
 foreach ($menu as &$menuItem) {
 	echo "<td><a href='{$site_root}$menuItem' id='$menuItem' class='lighten'><img alt='".ucfirst($menuItem)."' src='{$site_root}images/icons/$menuItem.png' width='100' height='100' style='display:block;'>".ucfirst($menuItem)."</a></td>";
 } ?>
