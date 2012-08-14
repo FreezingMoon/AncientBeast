f60c205b74a975c25fee94e5d55f41700046b3fb
Dread Knight <dk.vali@gmail.com>
2012-08-14 19:15:25 +0300

    now focus on creature is done at the beginning of the card, fixes #62

--- bestiary/cards.php
+++ bestiary/cards.php
@@ -84,11 +84,12 @@ function cards($id) { //Print a card
 		}
 	
 	//Card entry
-	echo "<center><table width=860px border=0><th class='card'>"; 
+
 	foreach ($ab_creatures_results as $r) {
 		$ab_name = $r['name'];
 		$spaceless = str_replace(" ", "_", $ab_name);
-		echo "<a name=\"$spaceless\"><table class='section'><tr class='beast'><td width='20%'>{$r['sin']}{$r['lvl']}</td><td><a href='#{$spaceless}'>".$r['name']."</a><td width='20%'>{$r['hex']}H</td></tr></table>";
+		echo "<center><a name=\"$spaceless\"></a><table width=860px border=0><th class='card'>"; 
+		echo "<table class='section'><tr class='beast'><td width='20%'>{$r['sin']}{$r['lvl']}</td><td><a href='#{$spaceless}'>".$r['name']."</a><td width='20%'>{$r['hex']}H</td></tr></table>";
 		echo "<a href=\"#grid\"><div class=\"section\" style=\"border: 0px; background:url('{$site_root}bestiary/{$r['name']}/artwork.jpg'); width:400px; height:400px;\"><img src=\"AB.png\" style=\"position:relative; top:365px; left:180px;\"></div></a>";
 		echo "<div class='section' style='text-align: center; width: 390px; padding: 5px 0px;'>{$r['description']}</div>";
 	}
