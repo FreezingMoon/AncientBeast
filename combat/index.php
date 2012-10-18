3282fb736ddb8f395a6f4275402e8d0754355fb7
Dread Knight <dk.vali@gmail.com>
2012-10-18 07:12:28 +0300

    added analytics to the game

--- combat/index.php
+++ combat/index.php
@@ -17,7 +17,20 @@
 		<script type="text/javascript" src="./js/script.js"></script>
 
 		<script type="text/javascript" src="../bestiary/Magma Spawn/abilities.js"></script>		
-		<script type="text/javascript" src="../bestiary/Dark Priest/abilities.js"></script>		
+		<script type="text/javascript" src="../bestiary/Dark Priest/abilities.js"></script>
+
+		<!--google analytics-->	
+		<script type="text/javascript">
+		var _gaq = _gaq || [];
+		_gaq.push(['_setAccount', 'UA-2840181-5']);
+		_gaq.push(['_trackPageview']);
+
+		(function() {
+			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		})();
+		</script>	
 	</head>
 	<body>
 		<div id="ui">
