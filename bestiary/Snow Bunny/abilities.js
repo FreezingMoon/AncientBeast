be80baccbcd577dfc66f721ef1bd3edbd51d895b
DreadKnight <dk.vali@gmail.com>
2012-11-02 20:40:39 +0200

    added Snow Bunny abilities template

---
+++ bestiary/Snow Bunny/abilities.js
@@ -0,0 +1,83 @@
+/*
+*
+*	Snow Bunny abilities
+*
+*/
+abilities["S1"] =[
+
+// 	First Ability
+{
+	//	Type : Can be "onQuery","onStartPhase","onDamage"
+	trigger : "onQuery",
+
+	//	Title
+	title : "Bunny Hopping",
+
+	//	Description
+	desc : "Avoids basic attack by moving to an available adjacent location.",
+
+	//	activate() : 
+	activate : function() {
+		//TODO
+
+	},
+},
+
+
+
+// 	Second Ability
+{
+	//	Type : Can be "onQuery","onStartPhase","onDamage"
+	trigger : "onQuery",
+
+	//	Title
+	title : "Big Nip",
+
+	//	Description
+	desc : "Dents nearby foe using it's big teeth.",
+
+	//	activate() : 
+	activate : function() {
+		//TODO
+	},
+},
+
+
+
+// 	Third Ability
+{
+	//	Type : Can be "onQuery","onStartPhase","onDamage"
+	trigger : "onQuery",
+
+	//	Title
+	title : "Blowing Wind",
+
+	//	Description
+	desc : "Pushes an inline creature several hexagons backwards, based on size.",
+
+	//	activate() : 
+	activate : function() {
+		//TODO
+	},
+},
+
+
+
+// 	Fourth Ability
+{
+	//	Type : Can be "onQuery","onStartPhase","onDamage"
+	trigger : "onQuery",
+
+	//	Title
+	title : "Chilling Spit",
+
+	//	Description
+	desc : "Spits inline foe with cold saliva. Bonus damage based on distance.",
+
+	//	activate() : 
+	activate : function() {
+		//TODO
+	},
+}
+
+];
