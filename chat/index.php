7fa5bc4a46730b7bb68b157c077dbdb1cc381331
DreadKnight <dk.vali@gmail.com>
2012-11-23 00:08:02 +0200

    refreshed IRC tutorial slide and also added IRC link etc, fixes #136 and #135

--- chat/index.php
+++ chat/index.php
@@ -27,13 +27,13 @@ require_once("../header.php");
 start_segment();
 ?>
 <span style="text-align: center; display: block; font-weight:bold;">
-<p><a href="mailto:DreadKnight@FreezingMoon.org" target="_blank">You might need to wait some time to get a response so consider sending us an <u>e-mail</u> instead.</p></a></span>
+<p><a href="irc://freenode.org/#AncientBeast" target="_blank"><u>Click here</u></a> to connect with your IRC client or use the <a href="#applet"><u>applet bellow</u></a>. You can <a href="mailto:DreadKnight@FreezingMoon.org" target="_blank"><u>email us</u></a> or <a href="#comment"><u>leave a comment</u></a>.</p></a></span>
 <?php
-separate_segment();
+separate_segment(applet);
 echo "<center><iframe src='http://webchat.freenode.net?channels=AncientBeast' width='99%' height='450'></iframe></center>";
-separate_segment();
+separate_segment(tutorial);
 echo "<center><iframe src='https://docs.google.com/present/embed?id=dfbbjc3c_24fb4cz7d8&size=l' frameborder='0' width='700' height='559'></iframe></center>";
-separate_segment();
+separate_segment(comment);
 include("../utils/disqus.php");
 end_segment();
 end_page();
