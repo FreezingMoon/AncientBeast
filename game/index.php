0d4135e3df084902d3e94b8a6f5f1160e0ca59e8
DreadKnight <dk.vali@gmail.com>
2013-01-30 07:04:24 +0200

    various tweaks, fixes #178 hopefully

--- game/index.php
+++ game/index.php
@@ -21,6 +21,10 @@
  * https://github.com/FreezingMoon/AncientBeast
  * DreadKnight@FreezingMoon.org
  */
+$style = '
+.strike {
+	text-decoration: line-through;
+}';
 
 $page_title = "Ancient Beast - Game";
 require_once("../header.php");
@@ -28,7 +32,7 @@ require_once("../global.php");
 start_segment();
 echo "<div class='center'><a href='https://github.com/FreezingMoon/AncientBeast' target='_blank'><b>This indie game project is under HEAVY development! Click here for issue tracker and github repository.</b></a></div>";
 separate_segment();
-echo "<div class='center'><a href='" . $site_root . "combat' target='_blank'><img src='launcher.jpg' width=890 height=400><h3 style='position:absolute; top:325px; left:100px;'>Launch Developer Preview [HotSeat]</h3></a></div>";
+echo "<div class='center shadow' style='margin:5px 0px;'><a href='" . $site_root . "combat' target='_blank'><img src='launcher.jpg' width=890 height=400><h3 style='position:absolute; top:325px; left:100px; background:rgba(0,0,0,0.5); border-radius:15px; border:4px ridge red; padding: 15px;'>Launch Developer Preview [HotSeat]</h3></a></div>";
 //separate_segment();
 //echo "<div class='center'>Arcade mode<br>1vs1/2vs2/spectate(watch others play or sign up to get notified of important scheduled matches); casual mode (fixed lvl 50, all creatures available but in common, no account required, no rewards, no items, no stats, no Godlet configuration), sinner mode (fixed lvl 50, full stack of creatures from a sin, no accoutn required, no rewards, no items, no stats, no Godlet configuration), trivia mode(fixed lvl 50, all creatures available but in common, no account required, no rewards, no items, no stats, no Godlet configuration); gambling (bet bitcoins on scheduled matches); Advanced mode<br>Ladder (rankings, normal exp and gold), Hardcore (one life, bonus exp and gold; if defeated, must purchase new life); Expert mode<br>Tournament (entry fee, estimated prize, status, rankings, scheduled matches, previous champions), Rankings (charts with classifications and scheduled matches; also previous champions listed), Challenge (fight the Sins and Ancient Beast)</div>";
 //TODO: move stuff above to just a pre-match screen; login box; provide video tutorial and showcase/explain various display modes and such, like braile/2d-static/2d-animated/3d/augmented-reality/virtual-reality/holographic/etc; link/QR code smartphone gamepad (image with smartphone = gamepad) as instalable app (it would need to be powered up by scanning a QR code); rankings; tournament planification; invite friends for rewards?; include recent change log?
@@ -44,9 +48,9 @@ separate_segment("shortcut"); ?>
 <li>256 MB RAM</li>
 <li>200 MB HDD</li>
 <li>64 MB Video Card</li>
-<li>controller (keyboard, mouse, gamepad, touch screen, smartphone or tablet)</li>
+<li>controller (<span class='strike'>keyboard</span>, mouse, <span class='strike'>gamepad</span>, touch screen, <span class='strike'>smartphone</span> or <span class='strike'>tablet</span>)</li>
 </ul>
-<p>Best played at Full HD resolution (1920x1080), with stereo speakers, using <a href="http://www.google.com/chrome" target="_blank"><u>Google Chrome</u></a>.<br>
+<p>Best played at Full HD resolution (1920x1080), <span class='strike'>with stereo speakers</span>, using <a href="http://www.google.com/chrome" target="_blank"><b>Google Chrome</b></a>.<br>
 While playing on a smartphone, it is highly recommended using a stylus.</p>
 <?php separate_segment();
 include("../utils/disqus.php");
