ceb5bfa8a2f10fefb33fbceff8087cc5901d2772
DreadKnight <dk.vali@gmail.com>
2013-08-05 10:59:39 +0300

    fixed epically noobish php array

--- profile/register.php
+++ profile/register.php
@@ -1,9 +1,8 @@
 Register
 <input type="text" name="username" placeholder="Username" autofocus>
 <input type="password" name="password" placeholder="Email" required>
-<input type="password" name="password" placeholder="Email" required>
 <input type="password" name="password" placeholder="Password" required>
-<input type="password" name="password" placeholder="Password" required>
+<input type="password" name="password" placeholder="Confirm" required>
 <button>Proceed</button>
 <?php
 
