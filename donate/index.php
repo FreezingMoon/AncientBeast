a566acc512edcea1d0cf0b1a7f7233b88ea24da5
DreadKnight <dk.vali@gmail.com>
2013-12-08 16:20:14 +0200

    fixed donation button images

--- donate/index.php
+++ donate/index.php
@@ -51,7 +51,7 @@ end_segment();
 <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
 <input type="hidden" name="cmd" value="_s-xclick">
 <input type="hidden" name="hosted_button_id" value="XXXPC6NDXBNTW">
-<input type="image" src="<?php echo $site_root; ?>/donate/dollar.png" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+<input type="image" src="dollar.png" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
 <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
 </form>
 <!-- $ one time (button) -->
@@ -93,7 +93,7 @@ end_segment();
 <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
 <input type="hidden" name="cmd" value="_s-xclick">
 <input type="hidden" name="hosted_button_id" value="N3D2HD7HUGX38">
-<input type="image" src="<?php echo $site_root; ?>/donate/euro.png" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+<input type="image" src="euro.png" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
 <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
 </form>
 <!-- € one time (button) -->
