9280b493c9c6a830ba6e46bbef1c8e2b36636497
DreadKnight <dk.vali@gmail.com>
2014-03-03 07:22:36 +0200

    some tweaks
    removed licensing stuff and updated charset encoding to html5

--- header.php
+++ header.php
@@ -1,32 +1,8 @@
-<?php
-/* Ancient Beast - Free Open Source Turn Based Strategy: card game meets chess, with creatures!
- * Copyright (C) 2007-2012  Valentin Anastase (a.k.a. Dread Knight).
- *
- * This file is part of Ancient Beast.
- *
- * Ancient Beast is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of
- * the License, or (at your option) any later version.
- *
- * Ancient Beast is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- *
- * http://www.AncientBeast.com
- * https://github.com/FreezingMoon/AncientBeast
- * DreadKnight@FreezingMoon.org
- */
-
-require_once('global.php'); ?>
+<?php require_once('global.php'); ?>
 <!doctype html>
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta charset="utf-8">
 <meta name="google-site-verification" content="uNVUkWjGPKwXaFK-BkOowVezLve8a8rjbM_ew1UHiZg">
 <meta name="description" content="Turn Based Strategy Game. Master your beasts!">
 <meta name="keywords" content="ancientbeast, ancient, beast, game, online, multiplayer, strategy, bitcoin, bets, combat, arena, chess, cards, creatures, free, open source, foss">
