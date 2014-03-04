b577f9963cb6329392cd0fea7436d51e531e5900
DreadKnight <dk.vali@gmail.com>
2014-03-04 07:04:55 +0200

    removed jquery
    this dependency was not needed; should improve website speed

--- header.php
+++ header.php
@@ -36,8 +36,6 @@ var site_root = "<?php echo 'http://'.$_SERVER['SERVER_NAME'].$site_root; ?>";
 	  echo '<link rel="stylesheet" href="' . $stylesheet . '">';
   } 
 ?>
-<!--jquery-->
-<script type="application/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
 <script type="application/javascript">
 $(document).ready(function(){
 	$('.lighten').each(function(){
