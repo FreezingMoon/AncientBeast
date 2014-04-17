d3ba25c3ec268d01ddf499fe4d5b306fb96476fc
DreadKnight <dk.vali@gmail.com>
2014-04-17 23:18:02 +0300

    updated Google Analytics code

--- header.php
+++ header.php
@@ -38,16 +38,15 @@ var site_root = "<?php echo 'http://'.$_SERVER['SERVER_NAME'].$site_root; ?>";
 ?>
 </title>
 <!--google analytics-->
-<script type="text/javascript">
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-2840181-5']);
-_gaq.push(['_trackPageview']);
+<script>
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-(function() {
-	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
+ga('create', 'UA-2840181-5', 'ancientbeast.com');
+ga('require', 'displayfeatures');
+ga('send', 'pageview');
 </script>
 </head>
 
