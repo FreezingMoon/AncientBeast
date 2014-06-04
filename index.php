95ab2107f1c93bb69f4725b7e04e41ea59c1df13
DreadKnight <dk.vali@gmail.com>
2014-06-04 18:44:58 +0300

    moved code to better place

--- index.php
+++ index.php
@@ -1,14 +1,33 @@
-<?php $style = '
+<?php
+/* Ancient Beast - Free Open Source Online PvP TBS: card game meets chess, with creatures.
+ * Copyright (C) 2007-2012  Valentin Anastase (a.k.a. Dread Knight)
+ *
+ * This file is part of Ancient Beast.
+ *
+ * Ancient Beast is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Ancient Beast is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * http://www.AncientBeast.com
+ * https://github.com/FreezingMoon/AncientBeast
+ * DreadKnight@FreezingMoon.org
+ */
+
+$style = '
 a.FM:hover { text-shadow: black 0.1em 0.1em 0.2em, blue 0 0 10px; }
 .image { cursor: pointer; display: inline-block; padding-left: 10px; }';
 
 require_once('header.php'); 
-
-if($_GET['action'] == 'logout'){
-  session_destroy();
- 		echo 'You have been logged out.';
-		 echo '<meta http-equiv="refresh" content="1;url=index.php">';
- } ?>
+?>
 <article>
 <div class="div" id="intro">
 <div style="width: 475px; text-align: justify; display:inline-block;">
