33a73cfab7226b7121c0c15ac84b6913f5d0cb8e
DreadKnight <dk.vali@gmail.com>
2014-06-04 18:41:43 +0300

    more donation widgets
    and some clean-up

--- donate/index.php
+++ donate/index.php
@@ -30,18 +30,18 @@ $style = "
 }";
 require_once("../header.php");
 
-$cancel = $_GET["cancel"];
-$success = $_GET["success"];
-
-if (isset($cancel)) { ?>
-<div class="div center"><b>It\'s a sad day. You have canceled...</b></div>
+// Post-donation feedback
+if (isset($_GET['cancel'])) { ?>
+<div class="warning center"><b>It's a sad day. You have canceled...</b></div>
 <?php
 }
 
-if (isset($success)) { ?>
-<div class="div center"><b>You are AWESOME! :)</b></div>
+if (isset($_GET['success'])) { ?>
+<div class="confirmation center"><b>You are AWESOME! :)</b></div>
 <?php
 }?>
+
+<!-- Golden div -->
 <a id="now"></a>
 <div style="padding:20px 0; margin-bottom:10px; background:rgba(0,0,0,0.5); border-radius:15px; border:4px ridge gold;">
 <table width=100%><tr><td align=center width=25%>
@@ -49,19 +49,20 @@ if (isset($success)) { ?>
 <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
 <input type="hidden" name="cmd" value="_s-xclick">
 <input type="hidden" name="hosted_button_id" value="XXXPC6NDXBNTW">
-<input type="image" src="dollar.png" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+<input type="image" src="dollar.png" border=0 name="submit" alt="PayPal - The safer, easier way to pay online!">
+<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width=1 height=1>
 </form>
 <!-- $ one time (button) -->
 <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
 <input type="hidden" name="cmd" value="_s-xclick">
 <input type="hidden" name="hosted_button_id" value="XXXPC6NDXBNTW">
-<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border=0 name="submit" alt="PayPal - The safer, easier way to pay online!">
+<img alt="" border=0 src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width=1 height=1>
 </form>
+</td>
 
-</td><td style="text-align:center;" width=25%>
 <!-- $ recursive -->
+<td style="text-align:center;" width=25%>
 <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
 <input type="hidden" name="cmd" value="_s-xclick">
 <input type="hidden" name="hosted_button_id" value="YDCXZ5QA5XH62">
@@ -82,28 +83,29 @@ if (isset($success)) { ?>
 <tr><td><input type="hidden" name="on2" value="What's your website?">What's your website?</td></tr><tr><td><input type="text" name="os2" maxlength="200"></td></tr>
 </table>
 <input type="hidden" name="currency_code" value="USD">
-<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_subscribe_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_subscribe_LG.gif" border=0 name="submit" alt="PayPal - The safer, easier way to pay online!">
+<img alt="" border=0 src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width=1 height=1>
 </form>
+</td>
 
-</td><td style="text-align:center;" width=25%>
 <!-- € one time (coin) -->
+<td style="text-align:center;" width=25%>
 <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
 <input type="hidden" name="cmd" value="_s-xclick">
 <input type="hidden" name="hosted_button_id" value="N3D2HD7HUGX38">
-<input type="image" src="euro.png" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+<input type="image" src="euro.png" border=0 name="submit" alt="PayPal - The safer, easier way to pay online!">
+<img alt="" border=0 src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width=1 height=1>
 </form>
 <!-- € one time (button) -->
 <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
 <input type="hidden" name="cmd" value="_s-xclick">
 <input type="hidden" name="hosted_button_id" value="N3D2HD7HUGX38">
-<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border=0 name="submit" alt="PayPal - The safer, easier way to pay online!">
+<img alt="" border=0 src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width=1 height=1>
 </form>
-
-</td><td style="text-align:center;" width=25%>
+</td>
 <!-- € recursive -->
+<td style="text-align:center;" width=25%>
 <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
 <input type="hidden" name="cmd" value="_s-xclick">
 <input type="hidden" name="hosted_button_id" value="9X7URD3Y7DTLC">
@@ -124,12 +126,14 @@ if (isset($success)) { ?>
 <tr><td><input type="hidden" name="on2" value="What's your website?">What's your website?</td></tr><tr><td><input type="text" name="os2" maxlength="200"></td></tr>
 </table>
 <input type="hidden" name="currency_code" value="EUR">
-<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_subscribe_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_subscribe_LG.gif" border=0 name="submit" alt="PayPal - The safer, easier way to pay online!">
+<img alt="" border=0 src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width=1 height=1>
 </form>
 
 </td></tr></table>
 </div>
+
+<!-- Total amounts -->
 <div class="div center">
 <?php
 $dollars_month = db_query('SELECT COALESCE(SUM(amount),0) AS amount FROM ab_donors WHERE type="$" AND MONTH(date) = MONTH(NOW()) AND YEAR(date) = YEAR(NOW())');
@@ -143,8 +147,10 @@ $euros_total = db_query('SELECT COALESCE(SUM(amount),0) AS amount FROM ab_donors
 <td align=center width=25%><u>' . date('F') . '</u><br><b>€' . $euros_month[0]['amount'] . ' EUR</b></td>
 <td align=center width=25%><u>Total</u><br><b>€' . $euros_total[0]['amount'] . ' EUR</b></td></tr></table></div>'; ?>
 
+<!-- Information -->
 <div class="div center">Donations are done through <a href="http://Paypal.com" target="_blank"><b>Paypal</b></a> and subscriptions can be canceled anytime, see <a href="http://www.wikihow.com/Cancel-a-Recurring-Payment-in-PayPal" target="_blank"><b>this article</b></a> for more info. Any amount you wish to donate is greatly appreciated and helps further the development of Ancient Beast. Donations are non refundable and are being used for project's upkeep, hardware necessities, rewarding existing contributors, commissioning artists and coders, marketing and so on. Donors are rewarded with exciting prizes!</div>
 
+<!-- List of donors -->
 <div class="div center">
 <?php
 $donors = 'SELECT * FROM ab_donors WHERE anonymous IS NULL ORDER BY amount DESC';
@@ -152,11 +158,25 @@ $rows = db_query($donors);
 foreach ($rows as $r) echo '<a href="' . $r['website'] . '" target="_blank">' . $r['name'] . ' (' . $r['type'] . $r['amount'] . ')</a>, '; ?>
 <a href="#now">Your name here!</a></div>
 
+<!-- Bitcoin donations -->
 <div class="div center">
 <p align=center>Feel free to send your <a href="http://www.bitcoin.org" style="font-weight: bold;" target="_blank"><img src="bitcoin.ico"> bitcoin</a> donation over at:</p>
 <p align=center style="width:380px; margin:0 auto; padding:20px 0; background:rgba(0,0,0,0.5); border-radius:15px; border:4px ridge gold;">
 <a href="bitcoin:1Gpa3NKn8nR9ipXPZbwkjYxqZX3cmz7q97?label=Ancient%20Beast"><img src="QR.png"><br><br>
 1Gpa3NKn8nR9ipXPZbwkjYxqZX3cmz7q97</a></p>
 <p align=center><a href="http://blockexplorer.com/address/1Gpa3NKn8nR9ipXPZbwkjYxqZX3cmz7q97" target="_blank">We like being transparent, so click <b>here</b> if you wish to see a list with all bitcoin donations.</a></p>
-<p align=center><iframe width="560" height="315" src="http://www.youtube.com/embed/Um63OQz3bjo" frameborder="0" allowfullscreen></iframe></p></div>
+<p align=center><iframe width=560 height=315 src="http://www.youtube.com/embed/Um63OQz3bjo" frameborder=0 allowfullscreen></iframe></p></div>
+
+<!-- Various donation widgets -->
+<!-- Flattr -->
+<div class="div center">
+<script type="application/javascript" src="https://api.flattr.com/js/0.6/load.js?mode=auto"></script>
+<div style="display:inline-block;"><a class="FlattrButton" style="display:none;" href="http://AncientBeast.com"></a></div>
+<!-- CentUp -->
+<div style="display:inline-block;" class="centup-top" data-url="http://AncientBeast.com" data-title="Strategy Game"></div>
+<script src="https://www.centup.org/button.js"></script>
+<!-- Gittip -->
+<script data-gittip-username="DreadKnight"
+        src="//gttp.co/v1.js"></script>
+</div>
 <?php include('../footer.php'); ?>
