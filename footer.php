cc0cda996cd26d907b76b103726c0d9c4e8853e8
DreadKnight <dk.vali@gmail.com>
2014-06-04 18:42:45 +0300

    patreon button instead of flattr
    http://www.patreon.com/AncientBeast

--- footer.php
+++ footer.php
@@ -1,18 +1,17 @@
 <div class="div"><table style="width:100%"><tr>
-<td><a href="<?php echo $site_url; ?>donate"><img src="<?php echo $site_url; ?>donate/paypal.png" height="63" width="56" alt="paypal"></a></td>
-<td><a href="bitcoin:1Gpa3NKn8nR9ipXPZbwkjYxqZX3cmz7q97?label=Ancient%20Beast"><img src="<?php echo $site_url; ?>donate/bitcoin.png" height="63" width="56" alt="bitcoin"></a></td>
-<script type="application/javascript" src="https://api.flattr.com/js/0.6/load.js?mode=auto"></script>
-<td><a class="FlattrButton" style="display:none;" href="http://AncientBeast.com"></a></td>
+<td><a href="<?php echo $site_url; ?>donate"><img src="<?php echo $site_url; ?>donate/paypal.png" height=61 width=55 alt="paypal"></a></td>
+<td><a href="bitcoin:1Gpa3NKn8nR9ipXPZbwkjYxqZX3cmz7q97?label=Ancient%20Beast"><img src="<?php echo $site_url; ?>donate/bitcoin.png" height=61 width=55 alt="bitcoin"></a></td>
+<td><a href="http://www.patreon.com/AncientBeast" target="_blank"><img src="<?php echo $site_url; ?>donate/patreon.png" height=61 width=55 alt="patreon"></a></td>
 <td style="width:50%"></td>
 <td>
 	<a href="http://FreezingMoon.org" target="_blank" class="lighten">
-	<img src="<?php echo $site_url; ?>images/Freezing_Moon.png" height="52" width="444" alt="Freezing Moon" class="lighten"></a>
+	<img src="<?php echo $site_url; ?>images/Freezing_Moon.png" height=52 width=444 alt="Freezing Moon" class="lighten"></a>
 	<a href="mailto:DreadKnight@FreezingMoon.org?Subject=You%20Rock!" target="_blank">Indie Game Projects - DreadKnight@FreezingMoon.org</a>
 </td>
 <td style="width:50%"></td>
-<td><a href="https://facebook.com/AncientBeast" target="_blank" class="lighten"><img src="<?php echo $site_url; ?>images/facebook.png" height="64" width="64" class="lighten" alt="facebook"></a></td>
-<td><a href="https://twitter.com/AncientBeast" target="_blank" class="lighten"><img src="<?php echo $site_url; ?>images/twitter.png" height="64" width="64" class="lighten" alt="twitter"></a></td>
-<td><a href="https://plus.google.com/+AncientBeast" target="_blank" class="lighten"><img src="<?php echo $site_url; ?>images/google.png" height="64" width="64" class="lighten" alt="google"></a></td>
+<td><a href="https://facebook.com/AncientBeast" target="_blank" class="lighten"><img src="<?php echo $site_url; ?>images/facebook.png" height=64 width=64 class="lighten" alt="facebook"></a></td>
+<td><a href="https://twitter.com/AncientBeast" target="_blank" class="lighten"><img src="<?php echo $site_url; ?>images/twitter.png" height=64 width=64 class="lighten" alt="twitter"></a></td>
+<td><a href="https://plus.google.com/+AncientBeast" target="_blank" class="lighten"><img src="<?php echo $site_url; ?>images/google.png" height=64 width=64 class="lighten" alt="google"></a></td>
 </tr></table></div>
 </div>
 </body>
