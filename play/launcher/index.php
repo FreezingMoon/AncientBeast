8b4299011746f9534ae548706d3bb04ef515c2c7
DreadKnight <dk.vali@gmail.com>
2014-06-11 11:03:25 +0300

    removed useless include

--- play/launcher/index.php
+++ play/launcher/index.php
@@ -12,7 +12,6 @@
 		<link rel="stylesheet" type="text/css" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.7.2/themes/smoothness/jquery-ui.css">
 
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-		<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
 		<script src="jquery.fullscreen-min.js"></script>
 
 		<script type="text/javascript">
