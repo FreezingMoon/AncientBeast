98704ff6e9c099b96050c47ee138553cede97d37
DreadKnight <dk.vali@gmail.com>
2014-06-11 09:39:44 +0300

    fixing play page
    if the config.php is missing, included global.php instead

--- combat/index.php
+++ combat/index.php
@@ -1,8 +1,6 @@
 <?php
-//Utility
-if(!file_exists(dirname(__FILE__) . "/../config.php"))
-	die("Warning: config.php not found, please edit config.php.in to point to a database and save it as config.php<br>Disclaimer: Since this project is web based, you can use the code and assets along with database.sql to host Ancient Beast yourself for testing and development purposes only! Also, your version should not be indexable by search engines because that can cause harm to the project!");
-require_once("../config.php"); // Needed for user accounts
+// Needed for user accounts
+require_once("../global.php");
 ?>
 <html>
 	<head>
