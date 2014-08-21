da42637d03ff890e2c072a63383b75f67814e977
DreadKnight <dk.vali@gmail.com>
2014-08-21 19:13:42 +0300

    units page licensing
    added some licensing for units page #790

--- units/index.php
+++ units/index.php
@@ -15,25 +15,41 @@ $creature_results = get_creatures();
 //echo '<nav>Modes: Normal | List | Versus - Log-in to purchase creatures. || You own x out of 50 creatures. - View: 2D/3D - SketchFab gallery</nav>';
 //get link php variable
 //if normal, list or versus...
-
-//grid view
 ?>
+<!-- License -->
+<div class="div" id="license">All the characters are under the <a href="http://creativecommons.org/licenses/by-sa/3.0/" target="_blank"><b>CC-BY-SA 3.0</b></a> license. You can use them however you like, even for commercial projects, as long as you credit / link the project and mention the original artist(s), kinda like in the next example: <i>from Ancient Beast game project http://AncientBeast.com - character (or artwork) created by Awesome_Artist</i></div>
+
+<!-- Grid view -->
+
 <style type="text/css">
-	#creaturegrid .vignette,
-	.vignette div.border,
-	.vignette div.overlay {
-		height: 128px;
-		width: 128px;
-	}
-	#creaturegrid {
-		width: 896px;
-		height: 896px;
-	}
+#creaturegrid .vignette,
+.vignette div.border,
+.vignette div.overlay {height: 128px; width: 128px;}
+#creaturegrid {width: 896px; height: 896px;}
+
+.progress-widget {background: url('../images/progress.png') no-repeat 0 -75px; width: 825px; height: 75px; margin-top: 10px;}
+.common-values {
+	background-image: url('../images/progress.png');
+	width: 75px;
+	height: 75px;
+	display: inline-block;
+}
+.progress-0 {background-image: none;}
+.progress-10 {background-position: -675px 0;}
+.progress-20 {background-position: -600px 0;}
+.progress-30 {background-position: -525px 0;}
+.progress-40 {background-position: -450px 0;}
+.progress-50 {background-position: -375px 0;}
+.progress-60 {background-position: -300px 0;}
+.progress-70 {background-position: -225px 0;}
+.progress-80 {background-position: -150px 0;}
+.progress-90 {background-position: -75px 0;}
+.progress-100 {background-position: 0 0;}
 </style>
 <?php creatureGrid($creature_results); ?>
 <br>
 <?php
-//detailed view
+// Detailed view
 $i = 0;
 foreach ($creature_results as $r) {
 	$underscore = str_replace(' ', '_', $r['name']);
