1561c95bd71aaeec2e7663b2c1d646842269c16e
DreadKnight <dk.vali@gmail.com>
2014-08-27 04:17:27 +0300

    various play page improvements
    less buttons, frame around game, no more recommended specs

--- play/index.php
+++ play/index.php
@@ -24,34 +24,60 @@
 $style = '
 .strike {
 	text-decoration: line-through;
-}';
+}
+#bar {
+	background: transparent;
+	width: 100%;
+	height: 25px;
+	position: relative;
+	margin-top: -35px;
+}
+#bar a {
+	text-decoration: none;
+	text-shadow: none;
+	font-weight: bold !important;
+	font-family: Verdana,Arial,sans-serif;
+	font-size: .9em !important;
+	padding: 2px 7px !important;
+	cursor: pointer;
+	border-radius: 4px;
+	border: 1px solid #d3d3d3;
+	color: #555555;
+	background: rgb(255,255,255); /* Old browsers */
+	background: linear-gradient(to bottom, rgba(255,255,255,1) 0%,rgba(241,241,241,1) 50%,rgba(225,225,225,1) 51%,rgba(246,246,246,1) 100%);
+}
+#bar a:hover {background: white; color: black;}
+iframe.fullscreen {padding-top: 0;}
+';
 
 $page_title = "Ancient Beast - Game";
 require_once('../header.php');
 ?>
 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script src="launcher/jquery.fullscreen-min.js"></script>
 
-<iframe id="game" name="game" src="launcher/" style="margin-top:-45px; width:950px; height:566px;" seamless webkitAllowFullScreen mozAllowFullScreen allowFullScreen></iframe>
+<script type="text/javascript">
+$(document).ready(function(){
+	$(document).bind("fullscreenchange", function() {
+		$('#game').toggleClass('fullscreen');
+	});
+});
+</script>
+
+<div id="bar">
+	<a onclick="if(confirm('Reset Game?')) var ifr=document.getElementsByName('game')[0]; ifr.src=ifr.src;" style="margin-left: 15px;">Reset Game</a>
+	<a onclick="$('#game').fullScreen(true)" style="margin-left: 682px;">Fullscreen</a>
+</div>
+<div class="center">
+	<iframe id="game" name="game" src="../combat/" style="border: 4px ridge; border-color: grey; width: 934px; height: 525px;" seamless webkitAllowFullScreen mozAllowFullScreen allowFullScreen></iframe>
+</div>
 <div class="div center" id="info">
-<a href="#comment-939192362"><b>Version 0.2 (released 15th June 2013)</b></a> - <a href="https://github.com/FreezingMoon/AncientBeast/issues/" target="_blank"><b>You can report all issues on Github</b></a> - <a href="https://github.com/FreezingMoon/AncientBeast/issues/milestones" target="_blank"><b>v0.3 ETA: 14th December 2013</b></a>
-There's no online multiplayer for now, but you can still play with friends by using a <b><a href="http://teamviewer.com" target="_blank">Screen Sharing Software</a></b>.
+Best played using a modern browser, <a href="https://www.google.com/chrome" target="_blank"><b>Google Chrome</b></a> recommended. <a href="<?php echo $site_root; ?>combat/" target="_blank"><b>Click here to open the game in new tab.</b></a><br>
+<a href="#comment-939192362"><b>Version 0.2 (released 15th June 2013)</b></a> - <a href="https://github.com/FreezingMoon/AncientBeast/issues/" target="_blank"><b>You can report all issues on Github</b></a> - <a href="https://github.com/FreezingMoon/AncientBeast/issues/milestones" target="_blank"><b>v0.3 ETA: 14th December 2014</b></a>
+There's no online multiplayer for now, but you can play online with friends by using a <b><a href="http://teamviewer.com" target="_blank">Screen Sharing Software</a></b>.
 <a href="<?php echo $site_root; ?>game/AncientBeast.html" download><b>Click here if you would like to download a desktop shortcut for this awesome game. Limited time offer!</b></a></div>
 
-<div class="div" id="requirements">
-<h3 class="indexheader"><a href="#requirements">Requirements</a></h3>
-
-<ul>
-<li>internet connection (offline version available soon)</li>
-<li>a modern browser, <a href="https://www.google.com/chrome" target="_blank"><b>Google Chrome</b></a> recommended</li>
-<li>16:9 full HD display recommended (game will adapt otherwise)
-<li>stereo speakers (optional)</li>
-<li>500 MHz Processor</li>
-<li>256 MB RAM</li>
-<li>200 MB HDD</li>
-<li>64 MB Video Card</li>
-<li>controller: keyboard, mouse or touch screen (soon: gamepad, smartphone, tablet, voice, gestures, mind)</li>
-</ul>
-</div>
 <?php
 disqus();
 include('../footer.php'); ?>
