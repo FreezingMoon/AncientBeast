007fcfc5c3d0ded959012866178c2c667fb82b25
DreadKnight <dk.vali@gmail.com>
2014-08-28 15:32:49 +0300

    fixing menu usage delay
    testing first patch

--- combat/index.php
+++ combat/index.php
@@ -12,8 +12,8 @@ require_once("../global.php");
 <link rel="stylesheet" type="text/css" href="./css/style.css">
 <link rel="stylesheet" type="text/css" href="./css/grid.css">
 
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
-<script type="text/javascript" src="./js/jquery-ui-1.9.2.custom.min.js"></script>
+<script data-cfasync="false" type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+<script data-cfasync="false" type="text/javascript" src="./js/jquery-ui-1.9.2.custom.min.js"></script>
 <script type="text/javascript" src="./js/phaser.min.js"></script>
 <script type="text/javascript" src="./js/jquery.transit.min.js"></script>
 <script type="text/javascript" src="./js/jquery.kinetic.js"></script>
