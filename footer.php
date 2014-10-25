4e2d8d582a1a14ad8c620f8b17e7f3469418b23e
DreadKnight <dk.vali@gmail.com>
2014-10-25 09:08:26 +0300

    footer tweaks
    fixed #799 and replaced paypal form witha simple link

--- footer.php
+++ footer.php
@@ -1,26 +1,22 @@
 <?php mysqli_close($link); ?>
-<div class="icon smalldiv center">
-<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_blank" style="float:left; margin-left:5px;">
-<input type="hidden" name="cmd" value="_s-xclick">
-<input type="hidden" name="hosted_button_id" value="CJF8R55CJE9R4">
-<input type="image" src="<?php echo $site_url; ?>images/footer/paypal.png" border="0" name="submit" class="icon lighten" alt="PayPal" title="Donate using PayPal!">
-</form>
-<a href="bitcoin:1Gpa3NKn8nR9ipXPZbwkjYxqZX3cmz7q97?label=Ancient%20Beast"><img src="<?php echo $site_url; ?>images/footer/bitcoin.png" class="icon lighten" alt="bitcoin" title="Donate some Bitcoins!"></a>
-<a href="https://www.bountysource.com/trackers/260151-ancient-beast"><img src="<?php echo $site_url; ?>images/footer/bountysource.png" class="icon lighten" alt="bountysource" title="Place some bounties!"></a>
-<a href="http://www.patreon.com/AncientBeast" target="_blank"><img src="<?php echo $site_url; ?>images/footer/patreon.png" class="icon lighten" alt="patreon" title="Donate via Patreon!"></a>	
-<a href="https://flattr.com/thing/126547/Ancient-Beast" target="_blank"><img src="<?php echo $site_url; ?>images/footer/flattr.png" class="icon lighten" alt="flattr" title="Donate via Flattr!"></a>
-<a href="http://steamcommunity.com/sharedfiles/filedetails/?id=195722359" target="_blank"><img src="<?php echo $site_url; ?>images/footer/steam.png" class="icon lighten" alt="steam" title="Steam"></a>
-<a href="http://www.indiedb.com/games/ancient-beast" target="_blank"><img src="<?php echo $site_url; ?>images/footer/indiedb.png" class="icon lighten" alt="indiedb" title="IndieDB"></a>
-<a href="http://gamejolt.com/games/arcade/ancient-beast/15964/" target="_blank"><img src="<?php echo $site_url; ?>images/footer/gamejolt.png" class="icon lighten" alt="gamejolt" title="Game Jolt"></a>
-<a href="https://facebook.com/AncientBeast" target="_blank"><img src="<?php echo $site_url; ?>images/footer/facebook.png" class="icon lighten" alt="facebook" title="Facebook"></a>
-<a href="https://twitter.com/AncientBeast" target="_blank"><img src="<?php echo $site_url; ?>images/footer/twitter.png" class="icon lighten" alt="twitter" title="Twitter"></a>
-<a href="https://plus.google.com/+AncientBeast" target="_blank"><img src="<?php echo $site_url; ?>images/footer/google.png" class="icon lighten" alt="google" title="Google Plus"></a>
-<a href="https://www.youtube.com/AncientBeastGame" target="_blank"><img src="<?php echo $site_url; ?>images/footer/youtube.png" class="icon lighten" alt="youtube" title="Youtube"></a>
-<a href="https://github.com/FreezingMoon/AncientBeast" target="_blank"><img src="<?php echo $site_url; ?>images/footer/github.png" class="icon lighten" alt="github" title="Github"></a>
-<a href="https://wuala.com/AncientBeast" target="_blank"><img src="<?php echo $site_url; ?>images/footer/wuala.png" class="icon lighten" alt="wuala" title="Wuala"></a>
-<a href="https://Ancient-Beast.deviantart.com" target="_blank"><img src="<?php echo $site_url; ?>images/footer/deviantart.png" class="icon lighten" alt="deviantart" title="deviantArt"></a>
-<a href="http://www.reddit.com/r/AncientBeast" target="_blank"><img src="<?php echo $site_url; ?>images/footer/reddit.png" class="icon lighten" alt="reddit" title="Reddit"></a>
-<a href="mailto:DreadKnight@FreezingMoon.org?Subject=You%20Rock!" target="_blank"><img src="<?php echo $site_url; ?>images/footer/contact.png" class="icon lighten" alt="contact" title="Contact"></a>
+<div class="footer smalldiv center">
+<a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=CJF8R55CJE9R4" target="_blank"><img src="<?php echo $site_url; ?>images/footer/paypal.png" class="lighten" alt="paypal" title="Donate using PayPal!"></a>
+<a href="bitcoin:1Gpa3NKn8nR9ipXPZbwkjYxqZX3cmz7q97?label=Ancient%20Beast"><img src="<?php echo $site_url; ?>images/footer/bitcoin.png" class="lighten" alt="bitcoin" title="Donate some Bitcoins!"></a>
+<a href="https://www.bountysource.com/trackers/260151-ancient-beast"><img src="<?php echo $site_url; ?>images/footer/bountysource.png" class="lighten" alt="bountysource" title="Place some bounties!"></a>
+<a href="http://www.patreon.com/AncientBeast" target="_blank"><img src="<?php echo $site_url; ?>images/footer/patreon.png" class="lighten" alt="patreon" title="Donate via Patreon!"></a>	
+<a href="https://flattr.com/thing/126547/Ancient-Beast" target="_blank"><img src="<?php echo $site_url; ?>images/footer/flattr.png" class="lighten" alt="flattr" title="Donate via Flattr!"></a>
+<a href="http://steamcommunity.com/sharedfiles/filedetails/?id=195722359" target="_blank"><img src="<?php echo $site_url; ?>images/footer/steam.png" class="lighten" alt="steam" title="Steam"></a>
+<a href="http://www.indiedb.com/games/ancient-beast" target="_blank"><img src="<?php echo $site_url; ?>images/footer/indiedb.png" class="lighten" alt="indiedb" title="IndieDB"></a>
+<a href="http://gamejolt.com/games/arcade/ancient-beast/15964/" target="_blank"><img src="<?php echo $site_url; ?>images/footer/gamejolt.png" class="lighten" alt="gamejolt" title="Game Jolt"></a>
+<a href="https://facebook.com/AncientBeast" target="_blank"><img src="<?php echo $site_url; ?>images/footer/facebook.png" class="lighten" alt="facebook" title="Facebook"></a>
+<a href="https://twitter.com/AncientBeast" target="_blank"><img src="<?php echo $site_url; ?>images/footer/twitter.png" class="lighten" alt="twitter" title="Twitter"></a>
+<a href="https://plus.google.com/+AncientBeast" target="_blank"><img src="<?php echo $site_url; ?>images/footer/google.png" class="lighten" alt="google" title="Google Plus"></a>
+<a href="https://www.youtube.com/AncientBeastGame" target="_blank"><img src="<?php echo $site_url; ?>images/footer/youtube.png" class="lighten" alt="youtube" title="Youtube"></a>
+<a href="https://github.com/FreezingMoon/AncientBeast" target="_blank"><img src="<?php echo $site_url; ?>images/footer/github.png" class="lighten" alt="github" title="Github"></a>
+<a href="https://wuala.com/AncientBeast" target="_blank"><img src="<?php echo $site_url; ?>images/footer/wuala.png" class="lighten" alt="wuala" title="Wuala"></a>
+<a href="https://Ancient-Beast.deviantart.com" target="_blank"><img src="<?php echo $site_url; ?>images/footer/deviantart.png" class="lighten" alt="deviantart" title="deviantArt"></a>
+<a href="http://www.reddit.com/r/AncientBeast" target="_blank"><img src="<?php echo $site_url; ?>images/footer/reddit.png" class="lighten" alt="reddit" title="Reddit"></a>
+<a href="mailto:DreadKnight@FreezingMoon.org?Subject=You%20Rock!" target="_blank"><img src="<?php echo $site_url; ?>images/footer/contact.png" class="lighten" alt="contact" title="Contact"></a>
 </div>
 </body>
 </html>
