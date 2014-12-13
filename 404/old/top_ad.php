0ae29befbb5df525de1e6625b9198fc0a9690d3c
DreadKnight <dk.vali@gmail.com>
2014-12-13 02:10:09 +0200

    clean up
    removed some old ad code

--- 404/old/top_ad.php
+++
@@ -1,9 +0,0 @@
-<script type="application/javascript"><!--
-google_ad_client = "ca-pub-9983172600670141";
-/* AB 404 top */
-google_ad_slot = "8602468648";
-google_ad_width = 728;
-google_ad_height = 90;
-//-->
-</script>
-<script type="application/javascript" src="http://pagead2.googlesyndication.com/pagead/show_ads.js"></script>
