2eb340c20ff26374da97f40b4dacf176f2124f21
DreadKnight <dk.vali@gmail.com>
2015-04-04 06:59:08 +0300

    fixed stat tooltips

--- images/stats/index.php
+++ images/stats/index.php
@@ -1,22 +1,41 @@
 <?php
-$stats = array("health","regrowth","endurance","energy","meditation","initiative","offense","defense","movement","pierce","slash","crush","shock","burn","frost","poison","sonic","mental");
+$stats = array(
+	'health',
+	'regrowth',
+	'endurance',
+	'energy',
+	'meditation',
+	'initiative',
+	'offense',
+	'defense',
+	'movement',
+	'pierce',
+	'slash',
+	'crush',
+	'shock',
+	'burn',
+	'frost',
+	'poison',
+	'sonic',
+	'mental'
+);
 
 foreach ($stats as $key => $x) {
-	$stats[$x] = '<img src="../images/stats/' . $x . '.png" class="icon" title="' . ucfirst($x) . '" onMouseOver="swap(this,\'mouseover_' . $x . '\')" onMouseOut="swap(this,\'normal_' . $x . '\')">'."\n";
-	define(strtoupper($x) . '_ICON', $stats[$x]);
+	$stats[$x] = '<img src="../images/stats/' . $x . '.png" class="icon" title="' . ucfirst($x) . '">'."\n";
 	unset($stats[$key]);
 }
 
-// Shows iconUsed by units/cards.php
+// Shows icons used by units/cards.php
 function displayStat($type = "health", $value = null, $modifiers = false) {
 	global $stats;
-	echo '<div stat="' . $type . '" class="stat ' . $type . '">';
+	echo '<div stat="' . $type . '" class="stat ' . $type . '" title="' . ucfirst($type) . '">';
 	echo '<div class="icon ' . $type . '"></div>';
 	if($value !== null) echo '<br><span class="value">' . $value . '</span>';
 	if($modifiers) echo '<div class="modifiers"><div>No active modifiers</div></div>';
 	echo '</div>';
 }
 ?>
+
 <style type="text/css">
 .icon { height: 32px; width: 32px; display: inline-block; }
 .stat { text-align: center; display: table-cell; vertical-align: middle; }
@@ -24,8 +43,8 @@ function displayStat($type = "health", $value = null, $modifiers = false) {
 <?php 
 foreach ($stats as $key => $value) {
 	echo '
-	.icon.' . $key . ', .' . $key . ' .icon {background-image: url("' . $site_root . 'images/stats/' . $key . '.png");}'
-	.'.icon.' . $key . ':hover, .' . $key . ':hover .icon {background-image: url("' . $site_root . 'images/stats/' . $key . '.gif");}
+	.icon.' . $key . ', .' . $key . ' .icon { background-image: url("' . $site_root . 'images/stats/' . $key . '.png"); }'
+	.'.icon.' . $key . ':hover, .' . $key . ':hover .icon { background-image: url("' . $site_root . 'images/stats/' . $key . '.gif"); }
 	';
 }
 ?>
