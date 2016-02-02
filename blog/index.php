5de462d0676765fe811ed2c984bbf0d7cb2ddaf9
DreadKnight <dk.vali@gmail.com>
2016-02-02 02:09:22 +0200

    hiring news + blenderart magazine issue
    plus small clean up

--- blog/index.php
+++ blog/index.php
@@ -34,6 +34,10 @@ require_once("../global.php");
 if there's a GET link variable, it should gather that and display that article only; this whole thing is to automise the blog and remove manual work and template redundancy -->
 <div class="center">
 	<div style="display: inline-block;" class="lighten">
+		<a href="2016-02-1"><img src="2016-02-1/thumb.jpg" class="frame"><br>
+		<div class="button" style="background-image: url(<?php echo $site_root; ?>images/push_button.png);">We Are Hiring A Coder</div></a>
+	</div>
+	<div style="display: inline-block;" class="lighten">
 		<a href="2013-06-15"><img src="2013-06-15/thumb.jpg" class="frame"><br>
 		<div class="button" style="background-image: url(<?php echo $site_root; ?>images/push_button.png);">Version 0.2 Released</div></a>
 	</div>
