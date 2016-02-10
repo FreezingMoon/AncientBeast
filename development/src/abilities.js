888238cd507ea45e0a8e7c5bd92b46b6b06b0729
DreadKnight <dk.vali@gmail.com>
2016-02-10 13:31:40 +0200

    more spacing

--- development/src/abilities.js
+++ development/src/abilities.js
@@ -3,8 +3,8 @@
 *	Class parsing function from creature abilities
 *
 */
-var Ability = Class.create({
-	initialize: function(creature,abilityID) {
+var Ability = Class.create( {
+	initialize: function(creature, abilityID) {
 		this.creature = creature;
 		this.used = false;
 		this.id = abilityID;
@@ -38,8 +38,8 @@ var Ability = Class.create({
 	*	End the ability. Must be called at the end of each ability function;
 	*
 	*/
-	end: function(desableLogMsg,deferedEnding) {
-		if(!desableLogMsg) G.log("%CreatureName"+this.creature.id+"% uses "+this.title);
+	end: function(desableLogMsg, deferedEnding) {
+		if(!desableLogMsg) G.log("%CreatureName" + this.creature.id + "% uses " + this.title);
 		this.applyCost();
 		this.setUsed(true); // Should always be here
 		G.UI.updateInfos(); // Just in case
@@ -58,7 +58,7 @@ var Ability = Class.create({
 	*
 	*/
 	setUsed: function(val) {
-		if(val){
+		if(val) {
 			this.used = true;
 			if(this.creature.id == G.activeCreature.id) // Avoid dimmed passive for current creature
 				G.UI.abilitiesButtons[this.id].changeState("disabled");
@@ -83,34 +83,34 @@ var Ability = Class.create({
 
 		// Gamelog Event Registration
 		if( G.triggers.onQuery.test(this.trigger) ) {
-			if(arguments[0] instanceof Hex){
-				var args = $j.extend({},arguments);
+			if(arguments[0] instanceof Hex) {
+				var args = $j.extend( {}, arguments);
 				delete args[0];
-				G.gamelog.add({action:"ability",target:{type:"hex",x:arguments[0].x,y:arguments[0].y},id:this.id,args:args});
+				G.gamelog.add( {action:"ability",target:{type: "hex", x: arguments[0].x, y: arguments[0].y }, id: this.id, args: args } );
 			}
 			if(arguments[0] instanceof Creature) {
-				var args = $j.extend({},arguments);
+				var args = $j.extend( {}, arguments);
 				delete args[0];
-				G.gamelog.add({action:"ability",target:{type:"creature",crea:arguments[0].id},id:this.id,args:args});
+				G.gamelog.add( { action: "ability", target: { type: "creature", crea: arguments[0].id }, id: this.id, args: args } );
 			}
 			if(arguments[0] instanceof Array) {
-				var args = $j.extend({},arguments);
+				var args = $j.extend( {}, arguments);
 				delete args[0];
 				var array = [];
-				arguments[0].each(function() { array.push({x:this.x,y:this.y}); });
-				G.gamelog.add({action:"ability",target:{type:"array",array:array},id:this.id,args:args});
+				arguments[0].each(function() { array.push( {x: this.x, y: this.y } ); });
+				G.gamelog.add( { action: "ability", target: { type: "array", array: array }, id: this.id, args: args } );
 			}
 		} else {
 			// Test for materialization sickness
 			if(this.creature.materializationSickness && this.affectedByMatSickness) return false;
 		}
 
-		return this.animation2({arg:arguments});
+		return this.animation2( { arg: arguments } );
 	},
 
 	animation2: function(o) {
 
-		var opt = $j.extend({
+		var opt = $j.extend( {
 			callback: function() {},
 			arg: {},
 		},o);
@@ -128,7 +128,7 @@ var Ability = Class.create({
 		p1 += (this.creature.player.flipped)? 5 : -5;
 		p2 += (this.creature.player.flipped)? -5 : 5;
 
-		if( !this.noAnimation ){
+		if( !this.noAnimation ) {
 			var anim_id = Math.random();
 
 			G.animationQueue.push(anim_id);
@@ -149,7 +149,7 @@ var Ability = Class.create({
 
 			setTimeout(function() {
 				if( !G.triggers.onAttack.test(ab.trigger) ) {
-					G.soundsys.playSound(G.soundLoaded[2],G.soundsys.effectsGainNode);
+					G.soundsys.playSound(G.soundLoaded[2], G.soundsys.effectsGainNode);
 					ab.activate.apply(ab,args);
 				}
 			},this.animation_datas.delay);
@@ -167,13 +167,13 @@ var Ability = Class.create({
 		}
 
 		var interval = setInterval(function() {
-			if(!G.freezedInput){
+			if(!G.freezedInput) {
 				clearInterval(interval);
 				opt.callback();
 			}
 		},100);
 
-		if( G.triggers.onAttack.test(ab.trigger) ) return ab.activate.apply(ab,args);
+		if( G.triggers.onAttack.test(ab.trigger) ) return ab.activate.apply(ab, args);
 	},
 
 
@@ -214,15 +214,16 @@ var Ability = Class.create({
 		var obj = obj || this.damages;
 		var string = "";
 
-		$j.each(obj,function(key,value) {
+		$j.each(obj,function(key, value) {
 
 			if(key == "special") {
-				string += value.replace(/%(health|regrowth|endurance|energy|meditation|initiative|offense|defense|movement|pierce|slash|crush|shock|burn|frost|poison|sonic|mental)%/g,'<span class="$1"></span>');
+				// TODO: don't manually list all the stats and masteries when needed
+				string += value.replace(/%(health|regrowth|endurance|energy|meditation|initiative|offense|defense|movement|pierce|slash|crush|shock|burn|frost|poison|sonic|mental)%/g, '<span class="$1"></span>');
 				return;
 			}
 
 			if(string !== "") string += ", ";
-			string += (value+'<span class="'+key+'"></span>');
+			string += (value+'<span class="' + key + '"></span>');
 		});
 		return string;
 	},
@@ -233,13 +234,14 @@ var Ability = Class.create({
 		for (var i = this.effects.length - 1; i >= 0; i--) {
 
 			if(this.effects[i].special) {
-				string += this.effects[i].special.replace(/%(health|regrowth|endurance|energy|meditation|initiative|offense|defense|movement|pierce|slash|crush|shock|burn|frost|poison|sonic|mental)%/g,'<span class="$1"></span>');
+				// TODO: don't manually list all the stats and masteries when needed
+				string += this.effects[i].special.replace(/%(health|regrowth|endurance|energy|meditation|initiative|offense|defense|movement|pierce|slash|crush|shock|burn|frost|poison|sonic|mental)%/g, '<span class="$1"></span>');
 				continue;
 			}
 
-			$j.each(this.effects[i],function(key,value) {
+			$j.each(this.effects[i],function(key, value) {
 			if(string !== "") string += ", ";
-			string += (value+'<span class="'+key+'"></span>');
+			string += (value+'<span class="' + key + '"></span>');
 		});
 		}
 
@@ -248,16 +250,16 @@ var Ability = Class.create({
 
 	/*	areaDamage(targets)
 	*
-	*	targets : Array : Example : target = [{target:crea1,hexsHit:2},{target:crea2,hexsHit:1}]
+	*	targets : Array : Example : target = [ { target: crea1, hexsHit: 2 }, { target: crea2, hexsHit: 1 } ]
 	*/
-	areaDamage : function(attacker,type,damages,effects,targets,notrigger) {
+	areaDamage : function(attacker, type, damages, effects, targets, notrigger) {
 		var multiKill = 0;
 		for (var i = 0; i < targets.length; i++) {
 			if(targets[i]===undefined) continue;
-			dmg = new Damage(attacker,type,damages,targets[i].hexsHit,effects);
+			dmg = new Damage(attacker, type, damages, targets[i].hexsHit, effects);
 			multiKill += (targets[i].target.takeDamage(dmg, notrigger).kill+0);
 		}
-		if(multiKill>1)	attacker.player.score.push({type:"combo",kills:multiKill});
+		if(multiKill>1)	attacker.player.score.push( { type: "combo", kills: multiKill } );
 	},
 
 	/*	atLeastOneTarget(hexs,team)
@@ -265,7 +267,7 @@ var Ability = Class.create({
 	*	hexs : Array : set of hex to test
 	*	team : String : ennemy, ally, both
 	*/
-	atLeastOneTarget : function(hexs,team) {
+	atLeastOneTarget : function(hexs, team) {
 		for (var i = 0; i < hexs.length; i++) {
 			if(hexs[i].creature instanceof Creature) {
 				var crea = hexs[i].creature;
@@ -322,17 +324,17 @@ var Ability = Class.create({
 
 		var r = this.requirements || this.costs;
 
-		var req = $j.extend(def,this.requirements);
+		var req = $j.extend(def, this.requirements);
 
 		// Plasma
 		if(req.plasma > 0 ) {
 			if( this.creature.player.plasma < req.plasma ) {
-				this.message = G.msg.abilities.notenough.replace("%stat%","plasma");
+				this.message = G.msg.abilities.notenough.replace("%stat%", "plasma");
 				return false;
 			}
 		}else if(req.plasma < 0 ) {
 			if( this.creature.player.plasma > -req.plasma ) {
-				this.message = G.msg.abilities.toomuch.replace("%stat%","plasma");
+				this.message = G.msg.abilities.toomuch.replace("%stat%", "plasma");
 				return false;
 			}
 		}
@@ -340,12 +342,12 @@ var Ability = Class.create({
 		// Energy
 		if(req.energy  > 0 ) {
 			if( this.creature.energy < req.energy ) {
-				this.message = G.msg.abilities.notenough.replace("%stat%","energy");
+				this.message = G.msg.abilities.notenough.replace("%stat%", "energy");
 				return false;
 			}
 		}else if(req.energy  < 0 ) {
 			if( this.creature.energy > -req.energy ) {
-				this.message = G.msg.abilities.toomuch.replace("%stat%","energy");
+				this.message = G.msg.abilities.toomuch.replace("%stat%", "energy");
 				return false;
 			}
 		}
@@ -353,12 +355,12 @@ var Ability = Class.create({
 		// Endurance
 		if(req.endurance  > 0 ) {
 			if( this.creature.endurance < req.endurance ) {
-				this.message = G.msg.abilities.notenough.replace("%stat%","endurance");
+				this.message = G.msg.abilities.notenough.replace("%stat%", "endurance");
 				return false;
 			}
 		}else if(req.endurance  < 0 ) {
 			if( this.creature.endurance > -req.endurance ) {
-				this.message = G.msg.abilities.toomuch.replace("%stat%","endurance");
+				this.message = G.msg.abilities.toomuch.replace("%stat%", "endurance");
 				return false;
 			}
 		}
@@ -366,12 +368,12 @@ var Ability = Class.create({
 		// Health
 		if(req.health  > 0 ) {
 			if( this.creature.health <= req.health ) {
-				this.message = G.msg.abilities.notenough.replace("%stat%","health");
+				this.message = G.msg.abilities.notenough.replace("%stat%", "health");
 				return false;
 			}
 		}else if(req.health  < 0 ) {
 			if( this.creature.health > -req.health ) {
-				this.message = G.msg.abilities.toomuch.replace("%stat%","health");
+				this.message = G.msg.abilities.toomuch.replace("%stat%", "health");
 				return false;
 			}
 		}
@@ -390,15 +392,15 @@ var Ability = Class.create({
 	applyCost : function() {
 		var crea = this.creature;
 		if( this.costs === undefined ) return;
-		$j.each(this.costs,function(key,value) {
+		$j.each(this.costs, function(key, value) {
 			if( typeof(value) == "number" ) {
 				if(key == 'health') {
 					crea.hint(value,'damage d'+value);
-					G.log("%CreatureName"+crea.id+"% loses "+value+" health");
+					G.log("%CreatureName" + crea.id + "% loses " + value + " health");
 				}
 				crea[key] = Math.max( crea[key]-value, 0 ); // Cap
 			}
-		});
+		} );
 		crea.updateHealth();
 		if( crea.id == G.activeCreature.id ) G.UI.energyBar.animSize( crea.energy/crea.stats.energy );
 	},
@@ -410,7 +412,7 @@ var Ability = Class.create({
 			flipped : this.creature.player.flipped,
 			x : this.creature.x,
 			y : this.creature.y,
-			directions : [1,1,1,1,1,1],
+			directions : [1, 1, 1, 1, 1, 1],
 			includeCrea : true,
 			stopOnCreature : true,
 			distance : 0,
@@ -434,33 +436,33 @@ var Ability = Class.create({
 
 				switch(i) {
 					case 0: // Upright
-						dir = G.grid.getHexMap(o.x+fx,o.y-8,0,o.flipped,diagonalup).reverse();
+						dir = G.grid.getHexMap(o.x+fx, o.y-8, 0, o.flipped, diagonalup).reverse();
 						break;
 					case 1: // StraitForward
-						dir = G.grid.getHexMap(o.x+fx,o.y,0,o.flipped,straitrow);
+						dir = G.grid.getHexMap(o.x+fx, o.y, 0, o.flipped, straitrow);
 						break;
 					case 2: // Downright
-						dir = G.grid.getHexMap(o.x+fx,o.y,0,o.flipped,diagonaldown);
+						dir = G.grid.getHexMap(o.x+fx, o.y, 0, o.flipped, diagonaldown);
 						break;
 					case 3: // Downleft
-						dir = G.grid.getHexMap(o.x+fx,o.y,-4,o.flipped,diagonalup);
+						dir = G.grid.getHexMap(o.x+fx, o.y, -4, o.flipped, diagonalup);
 						break;
 					case 4: // StraitBackward
-						dir = G.grid.getHexMap(o.x+fx,o.y,0,!o.flipped,straitrow);
+						dir = G.grid.getHexMap(o.x+fx, o.y, 0, !o.flipped, straitrow);
 						break;
 					case 5: // Upleft
-						dir = G.grid.getHexMap(o.x+fx,o.y-8,-4,o.flipped,diagonaldown).reverse();
+						dir = G.grid.getHexMap(o.x+fx, o.y-8, -4, o.flipped, diagonaldown).reverse();
 						break;
 					default:
 						break;
 				}
 
-				if( o.distance > 0 ) dir = dir.slice(0,o.distance+1);
+				if( o.distance > 0 ) dir = dir.slice(0, o.distance+1);
 
-				choices = choices.concat(dir.filterCreature(o.includeCrea,o.stopOnCreature,o.id,o.team));
+				choices = choices.concat(dir.filterCreature(o.includeCrea, o.stopOnCreature, o.id, o.team));
 			}
 		}
-		return this.atLeastOneTarget(choices,o.team);
+		return this.atLeastOneTarget(choices, o.team);
 	},
 
 
@@ -471,7 +473,7 @@ abilities = []; // Array containing all javascript methods for abilities
 /*
 * Damage Class
 */
-var Damage = Class.create({
+var Damage = Class.create( {
 
 	/* Constructor(amount,type,effects)
 	*
@@ -481,7 +483,7 @@ var Damage = Class.create({
 	*	area :	Integer : Number of hexagons being hit
 	*	effects :	Array : Contains Effect object to apply to the target
 	*/
-	initialize: function(attacker,type,damages,area,effects) {
+	initialize: function(attacker, type, damages, area, effects) {
 		this.attacker = attacker;
 		this.type = type;
 		this.damages = damages;
@@ -501,19 +503,19 @@ var Damage = Class.create({
 		var returnObj = {total:0};
 
 		// DAMAGE CALCULATION
-		$j.each(this.damages,function(key,value) {
+		$j.each(this.damages,function(key, value) {
 			if(key=="pure") { // Bypass defense calculation
 				var points = value;
 			} else {
 				var points = Math.round(value * (1 + (atk.offense - trg.defense / dmg.area + atk[key] - trg[key] )/100));
 				//For Debuging
-				if( G.debugMode ) console.log("damage = "+value+key+"dmg * (1 + ("+atk.offense+"atkoffense - "+trg.defense+"trgdefense / "+dmg.area+"area + "+atk[key]+"atk"+key+" - "+trg[key]+"trg"+key+" )/100)");
+				if( G.debugMode ) console.log("damage = " + value + key + "dmg * (1 + (" + atk.offense + "atkoffense - " + trg.defense + "trgdefense / " + dmg.area + "area + " + atk[key] + "atk" + key + " - " + trg[key] + "trg" + key + " )/100)");
 			}
 			returnObj[key] = points;
 			returnObj.total += points;
 		});
 
-		returnObj.total = Math.max(returnObj.total,1); // Minimum of 1 damage
+		returnObj.total = Math.max(returnObj.total, 1); // Minimum of 1 damage
 
 		return returnObj;
 	},
@@ -528,7 +530,7 @@ var Damage = Class.create({
 /*
 * Effect Class
 */
-var Effect = Class.create({
+var Effect = Class.create( {
 
 	/* Constructor(owner,parent,trigger,effectFn)
 	*
@@ -537,7 +539,7 @@ var Effect = Class.create({
 	*	trigger :	String : Event that trigger the effect
 	*	effectFn :	Function : Function to trigger
 	*/
-	initialize: function(name,owner,target,trigger,optArgs) {
+	initialize: function(name, owner, target, trigger, optArgs) {
 		this.id = effectId++;
 
 		this.name = name;
@@ -546,9 +548,9 @@ var Effect = Class.create({
 		this.trigger = trigger;
 		this.creationTurn = G.turn;
 
-		var args = $j.extend({
+		var args = $j.extend( {
 			// Default Arguments
-			requireFn : function() {return true;},
+			requireFn : function() { return true; },
 			effectFn : function() {},
 			alterations : {},
 			turnLifetime : 0,
@@ -564,22 +566,22 @@ var Effect = Class.create({
 	},
 
 	animation: function() {
-		this.activate.apply(this,arguments);
+		this.activate.apply(this, arguments);
 	},
 
 	activate: function(arg) {
 		if( !this.requireFn(arg) ) return false;
-		if( !this.noLog ) console.log("Effect "+this.name+" triggered");
+		if( !this.noLog ) console.log("Effect " + this.name + " triggered");
 		this.effectFn(this,arg);
 	},
 
 	deleteEffect: function() {
 		var i = this.target.effects.indexOf(this);
-		this.target.effects.splice(i,1);
+		this.target.effects.splice(i, 1);
 		i = G.effects.indexOf(this);
-		G.effects.splice(i,1);
+		G.effects.splice(i, 1);
 		this.target.updateAlteration();
-		console.log("Effect "+this.name+" deleted");
+		console.log("Effect " + this.name + " deleted");
 	},
 
 });
