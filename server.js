55b080a4f44d64c7f0fbf9f76a2526a1dd380115
DreadKnight <dk.vali@gmail.com>
2017-03-15 03:42:41 +0200

    tab consistency

--- server.js
+++ server.js
@@ -10,39 +10,39 @@ var qManager = require('./server/queuemanager.js');
 
 // Setup the game queue and connection details
 io.on('connection', function(session) {
-  console.log('a user connected');
+	console.log('a user connected');
 
-  // Store the username in the socket session for this client
-  var username = makeid();
-  session.username = username;
+	// Store the username in the socket session for this client
+	var username = makeid();
+	session.username = username;
 
-  // Add user to the queue
-  qManager.addToQueue(session);
+	// Add user to the queue
+	qManager.addToQueue(session);
 
-  session.on('disconnect', function() {
-    console.log('user disconnected');
-    qManager.removeFromQueue(session);
-  });
+	session.on('disconnect', function() {
+		console.log('user disconnected');
+		qManager.removeFromQueue(session);
+	});
 
-  // Send user the username
-  session.emit('login', session.username);
+	// Send user the username
+	session.emit('login', session.username);
 
 });
 
 
 function makeid() {
-    var text = "";
-    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	var text = "";
+	var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-    for( var i=0; i < 5; i++ )
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
+	for( var i=0; i < 5; i++ )
+		text += possible.charAt(Math.floor(Math.random() * possible.length));
 
-    return text;
+	return text;
 }
 
 
 // Listen for server, and use static routing for deploy directory
 server.listen(port, function () {
-  console.log('Server listening at port %d', port);
+	console.log('Server listening at port %d', port);
 });
 app.use(express.static('./deploy'));
