01d8de6efd38e23c645085bf8eb98913cd97531b
Karl Tiedt <ktiedt@gmail.com>
2018-03-19 02:06:20 -0500

    dependency updates to add prettier and eslint support -- pretty currently runs on precommit, eslint does not #962

--- webpack.config.js
+++ webpack.config.js
@@ -8,61 +8,72 @@ const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const production = process.env.production;
 
 const baseSettings = {
-    entry: path.resolve(__dirname, 'src', 'script.js'),
-    output: {
-        path: path.resolve(__dirname, 'deploy/'),
-        filename: 'ancientbeast.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.less$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'less-loader',
-                ]
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                ]
-            },
-            {
-                test: /\.(png|jpg|gif|svg|ogg|ico|cur|woff|woff2)$/,
-                use: [
-                    'file-loader'
-                ]
-            }
-        ]
-    },
-    resolve: {
-        alias: {
-            assets: path.resolve(__dirname, 'assets/'),
-            modules: path.join(__dirname, "node_modules")
-        }
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, 'src', 'index.html'),
-            favicon: path.resolve(__dirname, 'assets', 'favicon.ico')
-        }),
-    ]
+	entry: path.resolve(__dirname, 'src', 'script.js'),
+	output: {
+		path: path.resolve(__dirname, 'deploy/'),
+		filename: 'ancientbeast.js'
+	},
+	optimization: {
+		splitChunks: {
+			cacheGroups: {
+				vendor: {
+					test: "vendor",
+					name: "vendor",
+					enforce: true
+				}
+			}
+		}
+	},
+	module: {
+		rules: [
+			{
+				test: /\.less$/,
+				use: [
+					'style-loader',
+					'css-loader',
+					'less-loader',
+				]
+			},
+			{
+				test: /\.css$/,
+				use: [
+					'style-loader',
+					'css-loader',
+				]
+			},
+			{
+				test: /\.(png|jpg|gif|svg|ogg|ico|cur|woff|woff2)$/,
+				use: [
+					'file-loader'
+				]
+			}
+		]
+	},
+	resolve: {
+		alias: {
+			assets: path.resolve(__dirname, 'assets/'),
+			modules: path.join(__dirname, "node_modules")
+		}
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: path.resolve(__dirname, 'src', 'index.html'),
+			favicon: path.resolve(__dirname, 'assets', 'favicon.ico')
+		})
+	]
 }
 
 const prodSettings = {
-    plugins: [
-        new UglifyJSPlugin(),
-        new webpack.DefinePlugin({
-            'process.env.NODE_ENV': JSON.stringify('production')
-        })
-    ]
+	plugins: [
+		new UglifyJSPlugin(),
+		new webpack.DefinePlugin({
+			'process.env.NODE_ENV': JSON.stringify('production')
+		})
+	]
 }
 
 const devSettings = {
-    devtool: 'cheap-module-eval-source-map'
+	devtool: 'cheap-module-eval-source-map'
 }
 
 // Create either a production or development build depending on the `production` env setting
