d928b84ed51886e75c8ca10535dd2d6acdb7d806
Marcel Odya <marcelodya@gmail.com>
2018-04-30 19:26:03 +0100

    Reworked log messages storing and displaying.
    The mesages are now stored as an array of objects.
    The same messages now stack.
    
    Resolves: #845

--- src/ui/chat.js
+++ src/ui/chat.js
@@ -1,5 +1,6 @@
 import * as $j from 'jquery';
-import * as time from '../utility/time';
+// Unused for now
+// import * as time from '../utility/time';
 import * as str from '../utility/string';
 
 export class Chat {
@@ -15,6 +16,7 @@ export class Chat {
 		this.$chat.bind('click', () => {
 			game.UI.chat.toggle();
 		});
+		this.messages = [];
 
 		$j('#combatwrapper, #toppanel, #dash, #endscreen').bind('click', () => {
 			game.UI.chat.hide();
@@ -34,21 +36,59 @@ export class Chat {
 		this.$content.parent().scrollTop(this.$content.height());
 	}
 
-	addMsg(msg, htmlclass) {
-		let time = new Date(new Date() - this.game.startMatchTime);
-		this.$content.append(
-			"<p class='" +
-				htmlclass +
+	getCurrentTime() {
+		let currentTime = new Date(new Date() - this.game.startMatchTime);
+		return (
+			str.zfill(currentTime.getUTCHours(), 2) +
+			':' +
+			str.zfill(currentTime.getMinutes(), 2) +
+			':' +
+			str.zfill(currentTime.getSeconds(), 2)
+		);
+	}
+
+	createHTMLTemplate(msgTime, msg, amount, ifOuter = true, htmlClass = '') {
+		if (ifOuter) {
+			return (
+				"<p class='" +
+				htmlClass +
 				"'><i>" +
-				str.zfill(time.getUTCHours(), 2) +
-				':' +
-				str.zfill(time.getMinutes(), 2) +
-				':' +
-				str.zfill(time.getSeconds(), 2) +
+				msgTime +
 				'</i> ' +
 				msg +
+				(amount > 1 ? ' [ ' + amount + 'x ]' : '') +
 				'</p>'
-		);
+			);
+		} else {
+			return '<i>' + msgTime + '</i> ' + msg + (amount > 1 ? ' [ ' + amount + 'x ]' : '');
+		}
+	}
+
+	addMsg(msg, htmlClass) {
+		let messagesNo = this.messages.length;
+		let currentTime = this.getCurrentTime();
+
+		// Check if the last message was the same as the current one
+		if (this.messages[messagesNo - 1] && this.messages[messagesNo - 1].message === msg) {
+			let lastMessage = this.messages[messagesNo - 1];
+			lastMessage.amount++;
+			lastMessage.time = currentTime;
+			$j(lastMessage.DOMObject).html(
+				this.createHTMLTemplate(currentTime, msg, lastMessage.amount, false)
+			);
+		} else {
+			this.messages.push({
+				message: msg,
+				amount: 1,
+				time: currentTime,
+				class: htmlClass,
+				DOMObject: $j.parseHTML(this.createHTMLTemplate(currentTime, msg, 1, true, htmlClass))
+			});
+			this.$content.append(this.messages[this.messages.length - 1].DOMObject);
+		}
+
+		//Append the last messages DOM object
+
 		this.$content.parent().scrollTop(this.$content.height());
 	}
 }
