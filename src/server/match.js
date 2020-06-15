aa1552f828056518507e5b751079e412849eeeb7
Ryan <rconley@medtouch.com>
2020-06-15 14:56:31 -0400

    lint fixes  #2

--- src/server/match.js
+++ src/server/match.js
@@ -20,13 +20,13 @@ export default class MatchI {
 	}
 	async matchJoin(session, client) {
 		var matchList = await client.listMatches(session);
-    var openMatch = _.findWhere(matchList, { size: 1 });
-    console.log(openMatch);
+		var openMatch = _.findWhere(matchList, { size: 1 });
+		console.log(openMatch);
 		if (openMatch) {
-			return this._socket.send({ match_join: { match_id: id } }).then((m) => {
-				console.log(`joined match ${openMatch.match_id}`, m);
-				return m;
-			});
+			// return this._socket.send({ match_join: { match_id: id } }).then((m) => {
+			// 	console.log(`joined match ${openMatch.match_id}`, m);
+			// 	return m;
+			// });
 		}
 	}
 }
