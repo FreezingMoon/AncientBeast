feeb59e0109f8e5150b853301f4ef692d9098773
Noura Zekry <nourazerky2001@gmail.com>
2020-06-24 14:19:59 +0200

    printing animation details

--- src/abilities/Abolished.js
+++ src/abilities/Abolished.js
@@ -256,9 +256,8 @@ export default (G) => {
 				// var inRangeCreatures = crea.hexagons[1].adjacentHex(1);
 				//this.creature.faceHex(this.creature.hexagons[2]);
 
-				console.log(ability.getTargets);
-
 				let range = crea.adjacentHexes(1);
+				console.log(ability.getTargets(range));
 
 				G.grid.queryHexes({
 					fnOnConfirm: function () {
