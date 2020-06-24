8b51b2ce6239a37f1370d03fbeaf62637a5decb6
Ryan <rconley@medtouch.com>
2020-06-24 15:19:13 -0400

    clean up match join

--- src/server/match.js
+++ src/server/match.js
@@ -3,8 +3,8 @@ import _ from 'underscore';
 export default class MatchI {
 	constructor(socket, game, session) {
 		this.game = game;
-		this._socket = socket;
-		this._session = session;
+		this.socket = socket;
+		this.session = session;
 		this.user = session.username;
 		this.player1 = null;
 		this.player2 = null;
@@ -42,24 +42,23 @@ export default class MatchI {
 	}
 
 	async matchCreate(s, c) {
-		let nm = await this._socket.send({ match_create: {} });
-		this.player1 = this._session.username;
+		let nm = await this.socket.send({ match_create: {} });
+		this.player1 = this.session.username;
 		this.props = nm;
-		return nm;
+    return Promise.resolve(nm);
 	}
 	async matchJoin(s, c) {
 		let nj;
-
+    this.player2 = this.session.username;
 		var matchList = await c.listMatches(s);
-		console.log(matchList);
 		var openMatch = _.findWhere(matchList.matches, { size: 1 });
 
 		if (openMatch) {
-			nj = await this._socket.send({ match_join: { match_id: openMatch.match_id } });
+			nj = await this.socket.send({ match_join: { match_id: openMatch.match_id } });
 			console.log(`joined match ${openMatch.match_id}`, nj);
 			this.props = nj;
-			this.player2 = this._session.username;
-			return nj;
+			this.player2 = this.session.username;
+      return Promise.resolve(nj);
 		}
 	}
 
