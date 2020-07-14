1af4aabfd6781c50e8684ff4a8cf49ad497149f0
DreadKnight <dk.vali@gmail.com>
2020-07-15 02:43:50 +0300

    big favicon as splash #1701

--- webpack.config.js
+++ webpack.config.js
@@ -72,6 +72,7 @@ module.exports = (env, argv) => {
 			new HtmlWebpackPlugin({
 				template: path.resolve(__dirname, 'src', 'index.ejs'),
 				favicon: path.resolve(__dirname, 'assets', 'favicon.png'),
+				faviconBig: path.resolve(__dirname, 'assets', 'splash.png'),
 				production,
 			}),
 		],
