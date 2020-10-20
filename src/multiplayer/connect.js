0825c077726bb5bb5f7980b573464074a7e399d7
DreadKnight <dk.vali@gmail.com>
2020-10-20 04:45:16 +0300

    reverted, I'm so confused

--- src/multiplayer/connect.js
+++ src/multiplayer/connect.js
@@ -3,7 +3,7 @@ import { Client } from '@heroiclabs/nakama-js/dist/nakama-js.esm';
 export default class Server {
 	constructor(game) {
 		this.client = new Client(process.env.key, process.env.ip, process.env.port);
-		this.useSSL = true;
+		this.useSSL = false;
 		this.verboseLogging = false;
 		this.createStatus = false;
 		this.game = game;
