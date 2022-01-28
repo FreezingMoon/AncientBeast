c93b695f3077cc73c78b5036d361694649890af4
Kyle Strout <62024550+KyleStrout@users.noreply.github.com>
2022-01-28 17:58:09 -0500

    Log Attribute Upgrades for Issue #1962 w/ fix

--- src/ability.js
+++ src/ability.js
@@ -199,7 +199,7 @@ export class Ability {
 
 		// Configure score update for player
 		// When the ability is upgraded, add a single score bonus unique to that ability
-		if (this.isUpgraded()) {
+		if (this.isUpgraded() && this.usesLeftBeforeUpgrade() == 0) {
 			game.log(this.title + ' has been upgraded');
 			// Upgrade bonus uniqueness managed by preventing multiple bonuses
 			// with the same ability ID (which is an index 0,1,2,3 into the creature's abilities) and the creature ID
