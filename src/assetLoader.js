e6f768f5da96234db2da9a08353cf8c49b885b86
Ibrahim-Benkhedda <63601488+Ibrahim-Benkhedda@users.noreply.github.com>
2023-01-05 14:12:12 +0000

    Improved performance and documentation for getUrl function
    ## Description:
    
    This PR improves the performance of the getUrl function by implementing a cache to store previous results. It also updates the JSDoc comments to provide more detailed documentation for the function.
    
    ## Changes:
    
    - Added a cache to store previous results.
    - Updated the JSDoc comments to provide more detailed documentation.
    
    ## Benefits:
    
    - Improved performance: The cache helps to reduce the number of function calls, which can improve the overall performance of the application.
    - Improved documentation: The updated JSDoc comments provide more information about the function, which will be helpful for other developers working on the project.

--- src/assetLoader.js
+++ src/assetLoader.js
@@ -1,36 +1,55 @@
 import Assets from './assets';
 
-/** getUrl
+// Cache to store previous results
+const cache = {};
+
+/**
+ * Retrieves the URL for a specific asset based on its path. This function maintains a cache of previous results to improve performance. If the result is already in the cache, it is returned immediately. Otherwise, the result is calculated using the `reduce` method and added to the cache for future use.
  *
- * @param {String | Array} path path to the string
- * @return {?} ? ?
+ * @param {string|string[]} path The path to the asset, either as a string or an array of strings.
+ * @return {string} The URL of the asset.
+ *
+ * @throws {Error} If the path is empty or if the asset cannot be found.
  */
+
 export function getUrl(path) {
-	if (typeof path === 'string') {
-		path = path.split('/');
-	}
+  // Convert path to an array if it is a string
+  if (typeof path === 'string') {
+    path = path.split('/');
+  }
 
-	// Check if path is empty
-	if (path.length === 0) {
-		throw new Error('Path cannot be empty');
-	}
+  // Check if path is empty
+  if (path.length === 0) {
+    throw new Error('Path cannot be empty');
+  }
 
-	// prev = children (starts with the assets)
-	// current = what we are looking at now
-	return path.reduce((prev, current) => {
-		const entity = prev.find((e) => e.id === current);
-		if (entity === undefined) {
-			throw new Error(`Could not find asset with path: ${path.join('/')}`);
-		}
+  // Check if the result is already in the cache
+  const cacheKey = path.join('/');
+  if (cache[cacheKey]) {
+    return cache[cacheKey];
+  }
 
-		if (entity.children) {
-			// If there are still children left, return the children
-			return entity.children;
-		} else if (entity.url) {
-			// When there are no more children left, return the url
-			return entity.url;
-		} else {
-			throw new Error('Entity is of wrong type: ' + entity);
-		}
-	}, Assets);
+  // prev = children (starts with the assets)
+  // current = what we are looking at now
+  const result = path.reduce((prev, current) => {
+    const entity = prev.find((e) => e.id === current);
+    if (entity === undefined) {
+      throw new Error(`Could not find asset with path: ${path.join('/')}`);
+    }
+
+    if (entity.children) {
+      // If there are still children left, return the children
+      return entity.children;
+    } else if (entity.url) {
+      // When there are no more children left, return the url
+      return entity.url;
+    } else {
+      throw new Error('Entity is of wrong type: ' + entity);
+    }
+  }, Assets);
+
+  // Add the result to the cache
+  cache[cacheKey] = result;
+
+  return result;
 }
