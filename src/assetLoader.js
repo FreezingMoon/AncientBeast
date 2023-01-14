905dcba3e5bf1229ed8841b93687971b50d60323
DreadKnight <dk.vali@gmail.com>
2023-01-14 16:06:59 +0200

    tabs instead of spaces

--- src/assetLoader.js
+++ src/assetLoader.js
@@ -13,43 +13,43 @@ const cache = {};
  */
 
 export function getUrl(path) {
-  // Convert path to an array if it is a string
-  if (typeof path === 'string') {
-    path = path.split('/');
-  }
+	// Convert path to an array if it is a string
+	if (typeof path === 'string') {
+		path = path.split('/');
+	}
 
-  // Check if path is empty
-  if (path.length === 0) {
-    throw new Error('Path cannot be empty');
-  }
+	// Check if path is empty
+	if (path.length === 0) {
+		throw new Error('Path cannot be empty');
+	}
 
-  // Check if the result is already in the cache
-  const cacheKey = path.join('/');
-  if (cache[cacheKey]) {
-    return cache[cacheKey];
-  }
+	// Check if the result is already in the cache
+	const cacheKey = path.join('/');
+	if (cache[cacheKey]) {
+		return cache[cacheKey];
+	}
 
-  // prev = children (starts with the assets)
-  // current = what we are looking at now
-  const result = path.reduce((prev, current) => {
-    const entity = prev.find((e) => e.id === current);
-    if (entity === undefined) {
-      throw new Error(`Could not find asset with path: ${path.join('/')}`);
-    }
+	// prev = children (starts with the assets)
+	// current = what we are looking at now
+	const result = path.reduce((prev, current) => {
+		const entity = prev.find((e) => e.id === current);
+		if (entity === undefined) {
+			throw new Error(`Could not find asset with path: ${path.join('/')}`);
+		}
 
-    if (entity.children) {
-      // If there are still children left, return the children
-      return entity.children;
-    } else if (entity.url) {
-      // When there are no more children left, return the url
-      return entity.url;
-    } else {
-      throw new Error('Entity is of wrong type: ' + entity);
-    }
-  }, Assets);
+		if (entity.children) {
+			// If there are still children left, return the children
+			return entity.children;
+		} else if (entity.url) {
+			// When there are no more children left, return the url
+			return entity.url;
+		} else {
+			throw new Error('Entity is of wrong type: ' + entity);
+		}
+	}, Assets);
 
-  // Add the result to the cache
-  cache[cacheKey] = result;
+	// Add the result to the cache
+	cache[cacheKey] = result;
 
-  return result;
+	return result;
 }
