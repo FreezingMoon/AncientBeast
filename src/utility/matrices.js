200ffc112213172ae1fe62c4096ebc6d957e6317
PAVLOS ARAMPATZIS <92672537+paulosaramp@users.noreply.github.com>
2023-02-12 17:18:23 +0000

    Bounty Hunter Passive and 1 new Matrix 1)added a new matrice, "allaround1hex" it's a matrix showing which hexes are directly next to a 1 hex unit 2)added the new js file for bounty hunter, passive is done, needs testing

--- src/utility/matrices.js
+++ src/utility/matrices.js
@@ -36,6 +36,15 @@ export const bellowrow = [
 
 bellowrow.origin = [0, 0];
 
+export const allaround1hex = [
+	[0, 0, 0, 0],
+	[0, 1, 1, 0],
+	[1, 0, 1, 0], //Origin line
+	[0, 1, 1, 0],
+];
+
+allaround1hex.origin = [2, 2];
+
 export const frontnback2hex = [
 	[0, 0, 0, 0],
 	[0, 1, 0, 1],
