af6344d5eaf2b8c495d6beb994c904adddc01cf1
Peter <petermoldovia@yahoo.ca>
2023-04-11 15:17:55 +0200

    Allow Jest to test DOM

--- jest.config.js
+++ jest.config.js
@@ -144,7 +144,7 @@ module.exports = {
 	// snapshotSerializers: [],
 
 	// The test environment that will be used for testing
-	// testEnvironment: "jest-environment-node",
+	testEnvironment: 'jest-environment-jsdom',
 
 	// Options that will be passed to the testEnvironment
 	// testEnvironmentOptions: {},
