56bd8914c6689cf32416a6b9fb21ebf513d15d4e
cyberbishop <alabiavin@gmail.com>
2023-04-16 21:35:20 +0100

    Fix Don't scroll page

--- src/script.ts
+++ src/script.ts
@@ -52,9 +52,22 @@ $j(() => {
 	locationSelector.eq(randomLocationIndex).prop('checked', true).trigger('click');
 
 	// Disable initial game setup until browser tab has focus
-
 	window.addEventListener('blur', G.onBlur.bind(G), false);
 	window.addEventListener('focus', G.onFocus.bind(G), false);
+
+	// Add event listener to disable scroll in pre-game
+	let preMatch = $j('#pre-match');
+	preMatch.attr('tabindex', '0'); // set tabindex to make element focusable
+
+	// Disable scroll
+	preMatch.on('wheel', (e) => {
+		e.preventDefault();
+	});
+
+	// Disable up/down keys
+	preMatch.on('keydown', (e) => {
+		e.preventDefault();
+	});
 
 	// Add listener for Fullscreen API
 	let fullscreen = new Fullscreen($j('#fullscreen'));
