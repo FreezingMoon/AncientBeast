f971f432ea87d371554db6bcb09f8129eb8af397
Peter Moldovia <petermolodiva@yahoo.ca>
2023-06-04 14:18:07 -0500

    package.json is single source of authority for version

---
+++ src/utility/version.ts
@@ -0,0 +1,10 @@
+import * as semver from 'semver';
+import packageInfo from '../../package.json';
+
+// NOTE: The version string is set by /package.json.
+
+export const full = packageInfo.version;
+export const major = semver.major(full);
+export const minor = semver.minor(full);
+export const patch = semver.patch(full);
+export const major_minor = [major, minor].join('.');
