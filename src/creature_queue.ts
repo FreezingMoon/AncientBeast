83e06354f8277b3a4dd9800adf51e7e93cf99f07
Peter <petermoldovia@yahoo.ca>
2023-07-16 12:16:31 +0200

    bugfix: fix creature delay reset and delays across rounds
    * Split creature.delay() into creature.hinder() and creature.delay()
    * creature.hinder() is called when an attacker delays the creature
    * creature.wait() is called when the player delays their own creature
    * simplified creature queue
    
    See #2158, #2275

--- src/creature_queue.ts
+++ src/creature_queue.ts
@@ -1,103 +1,52 @@
-import Game from './game';
 import { Creature } from './creature';
-import * as arrayUtils from './utility/arrayUtils';
 
 export class CreatureQueue {
-	game: Game;
+	private _getCreatures: () => Creature[];
 	queue: Creature[];
 	nextQueue: Creature[];
-	tempCreature: Creature | Record<string, never>;
 
-	constructor(game: Game) {
-		this.game = game;
+	constructor(getCreatures: () => Creature[]) {
+		this._getCreatures = getCreatures;
 		this.queue = [];
 		this.nextQueue = [];
-		this.tempCreature = {};
-	}
-
-	/**
-	 * Add a creature to the next turn's queue by initiative, and optionally the current
-	 * turn's queue.
-	 *
-	 * @param {Creature} creature The creature to add.
-	 * @param {boolean} alsoAddToCurrentQueue Also add the creature to the current
-	 * turn's queue. Doing so lets a creature act in the same turn it was summoned.
-	 */
-	addByInitiative(creature: Creature, alsoAddToCurrentQueue = true) {
-		const queues = [this.nextQueue];
-
-		if (alsoAddToCurrentQueue) {
-			queues.push(this.queue);
-		}
-
-		queues.forEach((queue) => {
-			for (let i = 0; i < queue.length; i++) {
-				const queueItem = queue[i];
-
-				if (queueItem.delayed || queueItem.getInitiative() < creature.getInitiative()) {
-					queue.splice(i, 0, creature);
-					return;
-				}
-			}
-
-			queue.push(creature);
-		});
-	}
-
-	dequeue() {
-		return this.queue.splice(0, 1)[0];
-	}
-
-	remove(creature: Creature) {
-		arrayUtils.removePos(this.queue, creature);
-		arrayUtils.removePos(this.nextQueue, creature);
-	}
-
-	// Removes temporary Creature from queue
-	removeTempCreature() {
-		if (this.tempCreature instanceof Creature) {
-			this.remove(this.tempCreature);
-		}
-	}
-
-	nextRound() {
-		// Copy next queue into current queue
-		this.queue = this.nextQueue.slice(0);
-		// Sort next queue by initiative (current queue may be reordered) descending
-		this.nextQueue = this.nextQueue.sort((a, b) => b.getInitiative() - a.getInitiative());
 	}
 
 	isCurrentEmpty() {
-		return this.queue.length === 0;
+		return this.getCurrentQueueLength() === 0;
 	}
 
-	isNextEmpty() {
-		return this.nextQueue.length === 0;
+	getCurrentQueueLength() {
+		return this._getCreatures().filter((c) => c.isInCurrentQueue).length;
 	}
 
-	delay(creature: Creature) {
-		// Find out if the creature is in the current queue or next queue; remove
-		// it from the queue and replace it at the end
-		const game = this.game,
-			inQueue = arrayUtils.removePos(this.queue, creature) || creature === game.activeCreature;
-		let queue = this.queue;
+	update() {
+		this._updateQueues();
+	}
 
-		if (!inQueue) {
-			queue = this.nextQueue;
-			arrayUtils.removePos(this.nextQueue, creature);
-		}
-		// Move creature to end of queue but in order w.r.t. other delayed creatures
-		for (let i = 0, len = queue.length; i < len; i++) {
-			if (!queue[i].delayed) {
-				continue;
-			}
+	private _updateQueues() {
+		this._updateCurrentQueue();
+		this._updateNextQueue();
+	}
 
-			if (queue[i].getInitiative() < creature.getInitiative()) {
-				queue.splice(i, 0, creature);
-				return;
-			}
-		}
+	private _updateCurrentQueue() {
+		const creatures = this._getCreatures().filter((c) => c.isInCurrentQueue);
+		const undelayed = creatures
+			.filter((c) => !c.isDelayed)
+			.sort((a, b) => b.getInitiative() - a.getInitiative());
+		const delayed = creatures
+			.filter((c) => c.isDelayed)
+			.sort((a, b) => b.getInitiative() - a.getInitiative());
+		this.queue = [].concat(undelayed, delayed);
+	}
 
-		queue.push(creature);
+	private _updateNextQueue() {
+		const creatures = this._getCreatures().filter((c) => c.isInNextQueue);
+		const undelayed = creatures
+			.filter((c) => !c.isDelayedInNextQueue)
+			.sort((a, b) => b.getInitiative() - a.getInitiative());
+		const delayed = creatures
+			.filter((c) => c.isDelayedInNextQueue)
+			.sort((a, b) => b.getInitiative() - a.getInitiative());
+		this.nextQueue = [].concat(undelayed, delayed);
 	}
 }
