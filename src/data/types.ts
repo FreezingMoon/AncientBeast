4b30a41912847b29d78cb97b5a7d5dee48aaa899
Allan Martinez <martinezallan123@gmail.com>
2023-07-24 12:39:00 -0700

    narrow the `CreatureType` type

--- src/data/types.ts
+++ src/data/types.ts
@@ -1,5 +1,7 @@
 import { unitData } from './units';
 
+export type UnitData = typeof unitData;
+
 // Enables autocomplete for `key`
 function getKeyValue<T extends object, K extends keyof T>(obj: T, key: K) {
 	return obj[key];
@@ -10,16 +12,21 @@ const realms = unitData.map((unit) => getKeyValue(unit, 'realm'));
 const unitNames = unitData.map((unit) => getKeyValue(unit, 'name'));
 const unitLevels = unitData.map((unit) => getKeyValue(unit, 'level'));
 
-// Create an array containing the possible `creature.type` combinations.
-// In `game.ts`: creature.type = realm.toUpperCase() + level,
-const creatureTypes = realms.map((realm) => {
-	for (let i in unitLevels) {
-		return `${realm}${unitLevels[i]}` as const;
-	}
-});
+/*
+ * A creature's `type` is defined as `creature.realm` + `creature.level`
+ * Example: Dark Priest has `type` '--'
+ *
+ * This type helper makes it possible to exclude creature `type` combinations that don't actually exist on a creature
+ * For example: The combinations '-1' and 'A-' currently are not in use by any creature
+ */
+type ExtractValidCreatureTypes<T extends UnitData> = {
+	[key in keyof T]: `${T[key]['realm']}${T[key]['level']}`;
+}[number];
 
 // Create unions from the various arrays
 export type UnitName = typeof unitNames[number];
 export type Realm = typeof realms[number];
 export type Level = typeof unitLevels[number];
-export type CreatureType = typeof creatureTypes[number];
+
+// Create a union of valid creature `type`s
+export type CreatureType = ExtractValidCreatureTypes<UnitData>;
