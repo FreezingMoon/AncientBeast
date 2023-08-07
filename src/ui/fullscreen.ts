28704973eebe6aa55fbb144657ad71032c2ac188
Peter <petermoldovia@yahoo.ca>
2023-08-07 21:34:22 +0200

    refactor: rename member/arguments, inline functions, add JSDoc

--- src/ui/fullscreen.ts
+++ src/ui/fullscreen.ts
@@ -1,47 +1,40 @@
 export class Fullscreen {
-	private fullscreenElement: HTMLElement;
+	private button: HTMLElement;
 
-	constructor(fullscreenElement: HTMLElement, fullscreenMode = false) {
-		this.fullscreenElement = fullscreenElement;
-		if (fullscreenMode) {
-			fullscreenElement.classList.add('fullscreenMode');
+	constructor(button: HTMLElement, isFullscreen = false) {
+		this.button = button;
+		if (isFullscreen) {
+			button.classList.add('fullscreenMode');
 		}
 	}
 
 	toggle() {
-		if (isNativeFullscreenAPIUse()) {
-			disableFullscreenLayout(this.fullscreenElement);
+		if (isAppInNativeFullscreenMode()) {
+			this.button.classList.remove('fullscreenMode');
+			this.button
+				.querySelectorAll('.fullscreen__title')
+				.forEach((el) => (el.textContent = 'FullScreen'));
 			document.exitFullscreen();
-		} else if (!isNativeFullscreenAPIUse() && window.innerHeight === screen.height) {
+		} else if (!isAppInNativeFullscreenMode() && window.innerHeight === screen.height) {
 			alert('Use F11 to exit fullscreen');
 		} else {
-			enableFullscreenLayout(this.fullscreenElement);
+			this.button.classList.add('fullscreenMode');
+			this.button
+				.querySelectorAll('.fullscreen__title')
+				.forEach((el) => (el.textContent = 'Contract'));
 			document.getElementById('AncientBeast').requestFullscreen();
 		}
 	}
 }
 
-const isNativeFullscreenAPIUse = () => {
+/**
+ * @returns {boolean} true if app is currently in [fullscreen mode using the native API](https://developer.mozilla.org/en-US/docs/Web/API/Fullscreen_API), else false.
+ */
+function isAppInNativeFullscreenMode(): boolean {
 	// NOTE: These properties were vendor-prefixed until very recently.
 	// Keeping vendor prefixes, though they make TS report an error.
 	return (
 		// @ts-expect-error 2551
 		document.fullscreenElement || document.webkitFullscreenElement || document.mozFullScreenElement
 	);
-};
-
-const disableFullscreenLayout = (fullscreenElement: HTMLElement) => {
-	fullscreenElement.classList.remove('fullscreenMode');
-	const labelNode = fullscreenElement.querySelector('.fullscreen__title');
-	if (labelNode) {
-		labelNode.textContent = 'Fullscreen';
-	}
-};
-
-const enableFullscreenLayout = (fullscreenElement: HTMLElement) => {
-	fullscreenElement.classList.add('fullscreenMode');
-	const labelNode = fullscreenElement.querySelector('.fullscreen__title');
-	if (labelNode) {
-		labelNode.textContent = 'Contract';
-	}
-};
+}
