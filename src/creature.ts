451ac425543ffa0d300ec416dd68656667ea6acb
turkiasma <asma.turki@medtech.tn>
2025-04-06 16:28:20 +0100

    fix issue 2129

--- src/creature.ts
+++ src/creature.ts
@@ -807,6 +807,21 @@ export class Creature {
 		});
 	}
 
+highlightCurrentHexesAsDashed() {
+	this.hexagons.forEach((hex) => {
+		hex.display.loadTexture(`hex_dashed_p${this.player.id}`);
+	});
+}
+
+
+clearDashedOverlayOnHexes() {
+	this.hexagons.forEach((hex) => {
+		hex.display.loadTexture(`hex_p${this.player.id}`);
+	});
+}
+
+	
+
 	/**
 	 * Face creature at given hex
 	 * @param{Hex | Creature} facefrom - Hex to face from
