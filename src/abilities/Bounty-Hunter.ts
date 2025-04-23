7fa230844fb5ea0c3263ea17b07b1d6cad73302e
euhrich23 <euhrichwork@gmail.com>
2025-04-22 19:44:31 -0500

    Implemented abilities 3 + 4 of Bounty Hunter

--- src/abilities/Bounty-Hunter.ts
+++ src/abilities/Bounty-Hunter.ts
@@ -168,259 +168,181 @@ export default (G: Game) => {
 			},
 		},
 
-		// 	Third Ability: Ground Ball
+		/** Third Ability:
+		 * Pistol Shot
+		 * costs 15 energy pts.
+		 * Delivers a medium ranged shot that can damage target up to 6 hexagons away.
+         * 20 pierce damage in any of 6 directions.
+         * Upgrade: Can be used twice per round.
+		 */
+	
 		{
 			//	Type : Can be "onQuery", "onStartPhase", "onDamage"
 			trigger: 'onQuery',
-
 			_targetTeam: Team.Enemy,
+
 
 			// 	require() :
 			require: function () {
-				if (!this.testRequirements()) {
+				if (!this.testRequirements()){
 					return false;
-				}
-
-				const bellowrow = matrices.bellowrow;
-				const straitrow = matrices.straitrow;
-
-				const swine = this.creature;
-				const hexes = arrayUtils
-					.filterCreature(
-						G.grid.getHexMap(swine.x, swine.y - 2, 0, false, bellowrow),
-						true,
-						true,
-						swine.id,
-					)
-					.concat(
-						arrayUtils.filterCreature(
-							G.grid.getHexMap(swine.x, swine.y, 0, false, straitrow),
-							true,
-							true,
-							swine.id,
-						),
-						arrayUtils.filterCreature(
-							G.grid.getHexMap(swine.x, swine.y, 0, false, bellowrow),
-							true,
-							true,
-							swine.id,
-						),
-						arrayUtils.filterCreature(
-							G.grid.getHexMap(swine.x, swine.y - 2, 0, true, bellowrow),
-							true,
-							true,
-							swine.id,
-						),
-						arrayUtils.filterCreature(
-							G.grid.getHexMap(swine.x, swine.y, 0, true, straitrow),
-							true,
-							true,
-							swine.id,
-						),
-						arrayUtils.filterCreature(
-							G.grid.getHexMap(swine.x, swine.y, 0, true, bellowrow),
-							true,
-							true,
-							swine.id,
-						),
-					);
-				if (
-					!this.atLeastOneTarget(hexes, {
-						team: this._targetTeam,
-					})
-				) {
-					return false;
-				}
-
-				return true;
+				} 
+				return this.timesUsedThisTurn < this._getUsesPerTurn();
 			},
 
 			// 	query() :
 			query: function () {
-				const bellowrow = matrices.bellowrow;
-				const straitrow = matrices.straitrow;
+				const ab  = this;
+				const cre = ab.creature;
+			
+				G.grid.queryDirection({
+					fnOnConfirm: (...args) => ab.animation(...args),
+					team:          this._targetTeam,
+					id:            cre.id,
+					flipped:       cre.player.flipped,
+					x:             cre.x,
+					y:             cre.y,
+					sourceCreature: cre,
+					directions:    [1,1,1,1,1,1],
+					distance:      6,
+					minDistance:   1,
+					requireCreature: false
+				  });
+			  },
 
-				const ability = this;
-				const swine = this.creature;
-
-				const choices = [
-					// Front
-					G.grid.getHexMap(swine.x, swine.y - 2, 0, false, bellowrow),
-					G.grid.getHexMap(swine.x, swine.y, 0, false, straitrow),
-					G.grid.getHexMap(swine.x, swine.y, 0, false, bellowrow),
-					// Behind
-					G.grid.getHexMap(swine.x, swine.y - 2, 0, true, bellowrow),
-					G.grid.getHexMap(swine.x, swine.y, 0, true, straitrow),
-					G.grid.getHexMap(swine.x, swine.y, 0, true, bellowrow),
-				];
-
-				choices.forEach(function (choice) {
-					arrayUtils.filterCreature(choice, true, true, swine.id);
-				});
-
-				G.grid.queryChoice({
-					fnOnConfirm: function () {
-						// eslint-disable-next-line
-						ability.animation(...arguments);
-					}, // fnOnConfirm
-					team: this._targetTeam,
-					requireCreature: 1,
-					id: swine.id,
-					flipped: swine.player.flipped,
-					choices: choices,
-				});
+			_getUsesPerTurn: function () {
+				// If upgraded, useable twice per turn
+				return this.isUpgraded() ? 2 : 1;
 			},
 
 			//	activate() :
-			activate: function (path) {
-				const ability = this;
-				ability.end();
-				G.Phaser.camera.shake(0.01, 60, true, G.Phaser.camera.SHAKE_HORIZONTAL, true);
-
-				const target = arrayUtils.last(path).creature;
-
-				// If upgraded, hits will debuff target with -1 meditation
-				if (this.isUpgraded()) {
-					const effect = new Effect(
-						'Ground Ball',
-						ability.creature,
-						target,
-						'onDamage',
-						{
-							alterations: {
-								meditation: -1,
-							},
-						},
-						G,
-					);
-					target.addEffect(effect);
-					G.log('%CreatureName' + target.id + "%'s meditation is lowered by 1");
+			activate: function (hexes, args) {
+				const clicked = args.hex;
+				const tgt     = clicked && clicked.creature;
+				if (!tgt) return;
+			  
+				// 1) Screen shake + deal damage
+				G.Phaser.camera.shake(0.01, 150, true,G.Phaser.camera.SHAKE_HORIZONTAL,true);
+				tgt.takeDamage(new Damage(this.creature, this.damages, 1, [], G));
+			  
+				// 2) Decide if this is the final shot 
+				const nextUseCount = this.timesUsedThisTurn + 1;
+				const maxUses      = this._getUsesPerTurn();
+				const isFinalShot  = nextUseCount >= maxUses;
+			  
+				if (!isFinalShot) {
+				  // ─── Intermediate shot ───
+				  // Tell end() not to disable the button permanently:
+				  this._disableCooldowns = true;
+				  this.end();
+				  // Turn cooldowns back on for the final shot
+				  this._disableCooldowns = false;
+				  G.grid.redoLastQuery();
+			  
+				} else {
+				  // ─── Final shot ───
+				  // A normal end() will apply cost, log, disable the button, and return to movement.
+				  this.end();
 				}
-
-				const damage = new Damage(
-					ability.creature, // Attacker
-					ability.damages, // Damage Type
-					1, // Area
-					[], // Effects
-					G,
-				);
-				target.takeDamage(damage);
-			},
+			  }
 		},
 
-		// 	Fourth Ability: Mud Bath
+		/** Fourth Ability:
+		 * Rifle Assassin
+		 * costs 35 energy pts.
+         * Very powerful long range attack that can strike up to 12 hexagons distance.
+         * 40 pierce damage in any of 6 directions.
+         * Upgrade: Half Damage Pierce.
+		 */ 
 		{
-			//	Type : Can be "onQuery", "onStartPhase", "onDamage"
-			trigger: 'onQuery',
+  		trigger: 'onQuery',
+  		_targetTeam: Team.Both,   
 
-			_energyNormal: 30,
-			_energySelfUpgraded: 10,
+  		require: function () {
+    		return this.testRequirements();
+  		},
 
-			require: function () {
-				// If ability is upgraded, self cast energy cost is less
-				if (this.isUpgraded()) {
-					this.requirements = {
-						energy: this._energySelfUpgraded,
-					};
-					this.costs = {
-						energy: this._energySelfUpgraded,
-					};
-				} else {
-					this.requirements = {
-						energy: this._energyNormal,
-					};
-					this.costs = {
-						energy: this._energyNormal,
-					};
-				}
-				return this.testRequirements();
-			},
-
-			// 	query() :
-			query: function () {
-				const ability = this;
-				const swine = this.creature;
-
-				// Check if the ability is upgraded because then the self cast energy cost is less
-				const selfOnly = this.isUpgraded() && this.creature.energy < this._energyNormal;
-
-				let hexes = [];
-				if (!selfOnly) {
-					// Gather all the reachable hexes, including the current one
-					hexes = G.grid.getFlyingRange(swine.x, swine.y, 50, 1, 0);
-				}
-				hexes.push(G.grid.hexes[swine.y][swine.x]);
-
-				G.grid.queryHexes({
-					fnOnCancel: function () {
-						G.activeCreature.queryMove();
-					},
-					fnOnConfirm: function () {
-						// eslint-disable-next-line
-						ability.animation(...arguments);
-					},
-					hexes: hexes,
-					hideNonTarget: true,
-				});
-			},
-
-			//	activate() :
-			activate: function (hex: Hex) {
-				const ability = this;
-				const swine = this.creature;
-
-				// If upgraded and cast on self, cost is less
-				const isSelf = hex.x === swine.x && hex.y === swine.y;
-				if (this.isUpgraded() && isSelf) {
-					this.requirements = {
-						energy: this._energySelfUpgraded,
-					};
-					this.costs = {
-						energy: this._energySelfUpgraded,
-					};
-				} else {
-					this.requirements = {
-						energy: this._energyNormal,
-					};
-					this.costs = {
-						energy: this._energyNormal,
-					};
-				}
-
-				ability.end();
-
-				const effects = [
-					new Effect(
-						'Slow Down',
-						ability.creature,
-						hex,
-						'onStepIn',
-						{
-							requireFn: function () {
-								if (!this.trap.hex.creature) {
-									return false;
-								}
-								return this.trap.hex.creature.type != 'A1';
-							},
-							effectFn: function (effect, creatureHexOrDamage) {
-								if (creatureHexOrDamage instanceof Creature) {
-									creatureHexOrDamage.remainingMove--;
-								}
-							},
-						},
-						G,
-					),
-				];
-
-				new Trap(hex.x, hex.y, 'mud-bath', effects, ability.creature.player, {}, G);
-				G.soundsys.playSFX('sounds/mudbath');
-				// Trigger trap immediately if on self
-				if (isSelf) {
-					// onCreatureMove is Spa Goggles' trigger event
-					// @ts-expect-error 2554
-					G.onCreatureMove(swine, hex);
-				}
-			},
+  		query: function() {
+			const ability = this;
+			const cre     = ability.creature;
+		  
+			G.grid.queryDirection({
+			  fnOnSelect:  ()       => {},                        
+			  fnOnConfirm: (...args) => ability.animation(...args),
+			  fnOnCancel:  ()       => G.activeCreature.queryMove(),
+		  			  team:           Team.Both,
+			  id:             cre.id,
+			  flipped:        cre.player.flipped,
+			  x:              cre.x,
+			  y:              cre.y,
+			  sourceCreature: cre,
+		  
+			  directions:   [1,1,1,1,1,1],
+			  distance:     12,
+			  minDistance:  1,
+			  stopOnCreature:true,
+			  requireCreature: true,
+		  
+			  // only turn these on once upgraded
+			  dashedHexesAfterCreatureStop: ability.isUpgraded(),
+			  dashedHexesDistance:          ability.isUpgraded() ? 12 : 0,
+			  dashedHexesUnderCreature:     ability.isUpgraded(),
+			});
 		},
+
+  		activate: function (hexes, args) {
+    		const ability = this;
+    		const cre     = ability.creature;
+    		const dir     = args.direction;
+    		ability.end();
+    		G.Phaser.camera.shake(0.01, 150, true,
+            G.Phaser.camera.SHAKE_HORIZONTAL, true);
+						  
+			const full   = ability.damages.pierce;       // 40
+    		const half   = Math.floor(full / 2);         // 20
+    		const double = full + half;                  // 60
+
+    		const line = G.grid
+  				.getHexLine(cre.x, cre.y, dir, cre.player.flipped)
+  				.slice(1, 1 + 12);
+
+		let first = null;
+		let second = null;
+		let sawFirstTwice = false;
+
+		for (const h of line) {
+  			const t = h.creature;
+  			if (!t) continue;
+
+  			if (!first) {
+    			// first time we see any creature
+    			first = t;
+    			continue;
+  			}
+
+  			// if same creature showing up again immediately after first,
+  			// we treat that as a double occupancy and stop
+  			if (t.id === first.id) {
+    			sawFirstTwice = true;
+    			break;
+  			}
+
+  			// otherwise it’s a second, different creature
+  			second = t;
+  			break;
+		}
+
+		if (first) {
+  			if (this.isUpgraded() && sawFirstTwice && first.size > 1) {
+				first.takeDamage(new Damage(cre, { pierce: full + half }, 1, [], G));
+  			} else {
+				first.takeDamage(new Damage(cre, { pierce: full }, 1, [], G));
+    		if (this.isUpgraded() && second) {
+				second.takeDamage(new Damage(cre, { pierce: half }, 1, [], G))
+    		}
+  			}
+		}
+  	},
+},
 	];
 };
