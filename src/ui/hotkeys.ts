fd9592259ad9875d6ba65df8ba9a4250b7fa92f4
TheOne001 <sa9504@srmist.edu.in>
2026-05-18 04:30:26 +0530

    Fix audio hotkey behavior

--- src/ui/hotkeys.ts
+++ src/ui/hotkeys.ts
@@ -110,9 +110,9 @@ export class Hotkeys {
 
 	pressA(event) {
 		if (event.shiftKey) {
-			this.ui.btnAudio.triggerClick();
+			this.ui.cycleAudioMode();
 		} else {
-			this.ui.dashopen && this.ui.gridSelectLeft();
+			this.ui.btnAudio.triggerClick();
 		}
 	}
 
