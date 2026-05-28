f78dd24db22631bca6aafd4d531de9d4e1ba0f32
DreadKnight <dk.vali@gmail.com>
2026-05-28 21:38:45 +0300

    fix: remove problematic JQuery interface declaration from global types

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,8 +1,5 @@
 // Make JQuery globally available for ESLint and TypeScript
-declare global {
-	// eslint-disable-next-line no-unused-vars
-	interface JQuery<T = HTMLElement> extends $j.fn.init {}
-}
+// (Removed problematic JQuery interface declaration)
 interface Window {
 	// Global debugging object added to `window.AB`.
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
